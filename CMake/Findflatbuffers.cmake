86d79bc525c0a679b7214fe78c14715bbf5f2e68
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-07-26 12:58:50 +0200

    Add License to Finder

--- CMake/Findflatbuffers.cmake
+++ CMake/Findflatbuffers.cmake
@@ -1,6 +1,5 @@
 # Find the flatbuffers schema compiler
 # Copyright 2014 Stefan.Eilemann@epfl.ch
-#
 # Output Variables:
 # * FLATC_EXECUTABLE the flatc compiler executable
 # * FLATBUFFERS_FOUND
@@ -8,6 +7,18 @@
 # Provides:
 # * FLATC_TARGET(Name <files>) creates the C++ headers for the given flatbuffer
 #   schema files. Returns the header files in ${Name}_OUTPUTS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 find_program(FLATC_EXECUTABLE NAMES flatc)
 find_path(FLATBUFFERS_INCLUDE_DIR NAMES flatbuffers/flatbuffers.h)
