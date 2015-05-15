7a1b77322b07804bf32cbdb34b6cba25b552e825
rw <me@rwinslow.com>
2015-05-15 12:20:20 -0700

    Update Python package description: license, descriptions.

--- python/setup.py
+++ python/setup.py
@@ -2,16 +2,15 @@ from setuptools import setup
 
 setup(
     name='flatbuffers',
-    version='0.1',
-    license='BSD',
+    version='2015.05.14.0',
+    license='Apache 2.0',
     author='FlatBuffers Contributors',
     author_email='me@rwinslow.com',
-    url='https://github.com/google/flatbuffers/python',
-    long_description=('Python runtime library and code generator for use with'
-                      'the Flatbuffers serialization format.'),
+    url='https://github.com/google/flatbuffers',
+    long_description=('Python runtime library for use with the Flatbuffers'
+                      'serialization format.'),
     packages=['flatbuffers'],
     include_package_data=True,
     requires=[],
-    description=('Runtime library and code generator for use with the '
-                 'Flatbuffers serialization format.'),
+    description='The FlatBuffers serialization format for Python',
 )
