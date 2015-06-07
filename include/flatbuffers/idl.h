2f761418138819bbe04be92b8595c52cca089ce8
Amol Deshpande <amol.deshpande@outlook.com>
2015-06-06 19:35:12 -0700

    option to generate one file for C#

--- include/flatbuffers/idl.h
+++ include/flatbuffers/idl.h
@@ -406,6 +406,7 @@ struct GeneratorOptions {
   bool prefixed_enums;
   bool include_dependence_headers;
   bool mutable_buffer;
+  bool one_file;
 
   // Possible options for the more general generator below.
   enum Language { kJava, kCSharp, kGo, kMAX };
@@ -418,6 +419,7 @@ struct GeneratorOptions {
                        output_enum_identifiers(true), prefixed_enums(true),
                        include_dependence_headers(false),
                        mutable_buffer(false),
+                       one_file(false),
                        lang(GeneratorOptions::kJava) {}
 };
 
