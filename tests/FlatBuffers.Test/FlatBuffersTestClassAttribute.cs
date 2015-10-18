9d66af6efc0498e90e2757659761ae0c4f797eee
evolutional <oli@evolutional.co.uk>
2015-10-18 17:08:39 -0400

    Ported some of the python fuzz tests to C#
    * Refactored the test runner to use attribute based test discovery
    * Ported value and vtable/object fuzzing tests from python to C#

---
+++ tests/FlatBuffers.Test/FlatBuffersTestClassAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlatBuffers.Test
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FlatBuffersTestClassAttribute : Attribute
+    {
+    }
+}
