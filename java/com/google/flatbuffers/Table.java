7a62fed8d0f1f8c80e8069746bde03da0a4718a9
Louis CAD <louis.cognault@gmail.com>
2016-08-22 18:17:46 +0200

    Add ability to create a default empty flatbuffer
    This allows to call the default constructor and not have a broken object anymore. This also drastically reduces boilerplate when creating a flatbuffer with default values.
    
    Solves issue #3945

--- java/com/google/flatbuffers/Table.java
+++ java/com/google/flatbuffers/Table.java
@@ -57,6 +57,7 @@ public class Table {
    * @return Returns an offset into the object, or `0` if the field is not present.
    */
   protected int __offset(int vtable_offset) {
+    if (bb == null) return 0;
     int vtable = bb_pos - bb.getInt(bb_pos);
     return vtable_offset < bb.getShort(vtable) ? bb.getShort(vtable + vtable_offset) : 0;
   }
