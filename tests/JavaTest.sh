309ffe5266f57827d24b96d6f53acac1df71be09
Romain Gilles <rgilles@github>
2016-08-29 11:26:31 +0200

    Isolate the complied Java classes into a target folder + log java version.
    It is a common practice to put all the compiled classes into a
    dedicated folder in order to:
     - avoid to mix the code and the complied classes
     - to allow a simple way to remove the complied classes
    
    Add log of java version in order to make it explicit to the end user.

--- tests/JavaTest.sh
+++ tests/JavaTest.sh
@@ -16,8 +16,12 @@
 
 echo Compile then run the Java test.
 
+java -version
+
 testdir=$(readlink -fn `dirname $0`)
 thisdir=$(readlink -fn `pwd`)
+
+targetdir=${testdir}/target
 
 if [[ "$testdir" != "$thisdir" ]]; then
 	echo error: must be run from inside the ${testdir} directory
@@ -25,5 +29,12 @@ if [[ "$testdir" != "$thisdir" ]]; then
 	exit 1
 fi
 
-javac -classpath ${testdir}/../java:${testdir}:${testdir}/namespace_test JavaTest.java
-java -classpath ${testdir}/../java:${testdir}:${testdir}/namespace_test JavaTest
+if [ -e "${targetdir}" ]; then
+    echo "clean target"
+    rm -fr ${targetdir}
+fi
+
+mkdir ${targetdir}
+
+javac -d ${targetdir} -classpath ${testdir}/../java:${testdir}:${testdir}/namespace_test JavaTest.java
+java -classpath ${targetdir} JavaTest
