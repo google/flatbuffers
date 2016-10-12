91eac49ea5b8ed78bdba689e3d8c7738dc5ab63f
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2016-10-12 11:49:49 +0200

    Manually fixed generation issue of compare bool.

--- tests/keysearch_test/Testing/KeySearch/BoolEntry.java
+++ tests/keysearch_test/Testing/KeySearch/BoolEntry.java
@@ -38,8 +38,8 @@ public final class BoolEntry extends Table {
 
   @Override
   protected int keysCompare(Integer o1, Integer o2, ByteBuffer _bb) {
-    boolean val_1 = _bb=bb.get(__offset(4, o1, _bb));
-    boolean val_2 = _bb=bb.get(__offset(4, o2, _bb));
+    byte val_1 = bb.get(__offset(4, o1, _bb));
+    byte val_2 = bb.get(__offset(4, o2, _bb));
     return val_1 > val_2 ? 1 : val_1 < val_2 ? -1 : 0;
   }
 }
