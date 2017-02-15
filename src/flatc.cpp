3a2d3a232f41ebd82528d3b6c885f3c4279a4e3f
Wouter van Oortmerssen <aardappel@gmail.com>
2017-02-15 14:59:25 -0800

    Removed spurious "aa" from flatc usage message.
    Change-Id: Ia1812d594882ce6c51002d07827ccec267d138bb

--- src/flatc.cpp
+++ src/flatc.cpp
@@ -46,7 +46,7 @@ void FlatCompiler::Error(const std::string &err, bool usage,
 
 std::string FlatCompiler::GetUsageString(const char* program_name) const {
   std::stringstream ss;
-  ss << "Usageaa: " << program_name << " [OPTION]... FILE... [-- FILE...]\n";
+  ss << "Usage: " << program_name << " [OPTION]... FILE... [-- FILE...]\n";
   for (size_t i = 0; i < params_.num_generators; ++i) {
     const Generator& g = params_.generators[i];
 
