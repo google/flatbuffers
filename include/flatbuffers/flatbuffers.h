2c0a96c4cf2426e15d861e15de7bf6d550f07d21
akamat-ibm <kamat@us.ibm.com>
2017-04-28 05:14:40 +0000

    changes to support s390x

--- include/flatbuffers/flatbuffers.h
+++ include/flatbuffers/flatbuffers.h
@@ -77,7 +77,7 @@
 // the common platforms).
 #if !defined(FLATBUFFERS_LITTLEENDIAN)
   #if defined(__GNUC__) || defined(__clang__)
-    #ifdef __BIG_ENDIAN__
+    #if defined(__BIG_ENDIAN__) || defined(__BIG_ENDIAN)
       #define FLATBUFFERS_LITTLEENDIAN 0
     #else
       #define FLATBUFFERS_LITTLEENDIAN 1
