85edf5ba37979b3c7b859075b9d63faf4fef227e
schoetbi <schoetbi@users.noreply.github.com>
2017-06-23 15:19:38 +0200

    JsonSchemaGenerator: Corrected generation of typenames Current problems: enum types not written correctly #4360

--- src/idl_gen_json_schema.cpp
+++ src/idl_gen_json_schema.cpp
@@ -24,109 +24,149 @@ namespace flatbuffers {
                                        const std::string &file_name) {
     return path + file_name + ".schema.json";
   }
-namespace jsons {
-  // Return a C++ type from the table in idl.h
-  std::string GenTypeBasic(const Type &type, bool user_facing_type) {
-    static const char *ctypename[] = {
-#define FLATBUFFERS_TD(ENUM, IDLTYPE, CTYPE, JTYPE, GTYPE, NTYPE, PTYPE) \
-            #CTYPE,
-      FLATBUFFERS_GEN_TYPES(FLATBUFFERS_TD)
-#undef FLATBUFFERS_TD
-    };
-    if (user_facing_type) {
-      if (type.base_type == BASE_TYPE_BOOL) return "bool";
-    }
-    return ctypename[type.base_type];
-  }
+  namespace jsons {
+    std::string GenNativeType(BaseType type) {
+      switch (type) {
+      case BASE_TYPE_BOOL:    return "boolean";
 
-  std::string GenTypeNative(const Type &type,
-                            const FieldDef &field) {
-    switch (type.base_type) {
-    case BASE_TYPE_STRING:
-    {
-      return "string";
-    }
-    case BASE_TYPE_VECTOR:
-    {
-      const auto type_name = GenTypeNative(type.VectorType(), field);
-      return "array";
-    }
-    case BASE_TYPE_STRUCT:
-    {
-      return "baseTypeStruct";
-    }
-    case BASE_TYPE_UNION:
-    {
-      return type.enum_def->name + "Union";
-    }
-    default:
-    {
-      return GenTypeBasic(type, true);
-    }
-    }
-  }
-
-  class JsonSchemaGenerator : public BaseGenerator {
-  private:
-    CodeWriter code_;
-  public:
-    JsonSchemaGenerator(const Parser& parser, const std::string& path, const std::string& file_name)
-      : BaseGenerator(parser, path, file_name, "", "") {
+      case BASE_TYPE_CHAR:
+      case BASE_TYPE_UCHAR:
+      case BASE_TYPE_SHORT:
+      case BASE_TYPE_USHORT:
+      case BASE_TYPE_INT:
+      case BASE_TYPE_UINT:
+      case BASE_TYPE_LONG:
+      case BASE_TYPE_ULONG:
+      case BASE_TYPE_FLOAT:
+      case BASE_TYPE_DOUBLE:
+        return "number";
+      case BASE_TYPE_STRING:  return "string";
+      default:
+      {
+        return "?";
+      }
+      }
     }
 
-    explicit JsonSchemaGenerator(const BaseGenerator& base_generator)
-      : BaseGenerator(base_generator) {
+    std::string GenTypeRef(const std::string &typeName) {
+      return "\"$ref\" : \"#/definitions/" + typeName + "\"";
     }
 
-    bool generate() override{
-      code_.Clear();
-      code_ += "{";
-      code_ += "\"$schema\": \"http://json-schema.org/draft-04/schema#\",";
-      code_ += "\"definitions\": {";
+    std::string GenType(const std::string &name) {
+      return "\"type\" : \"" + name + "\"";
+    }
 
-      for (auto &s : parser_.structs_.vec) {
-        code_ += "";
-        code_ += "\"" + s->name + "\" : {";
-        code_ += "\"type\" : \"object\",";
-        std::stringstream comment;
-        for (auto commentLine : s->doc_comment) {
-          comment << commentLine;
-        }
-        code_ += "\"description\" : \"" + comment.str() +"\",";
-        code_ += "\"properties\" : {";
-
-        for (auto const &prop : s->fields.vec) {
-          std::string typeLine("  \"" + prop->name + "\" : { \"type\" : \"" + GenTypeBasic(prop->value.type, true) + "\" }");
-          if (&prop != &s->fields.vec.back()) {
-            code_ += typeLine + ",";
-          }
-          else {
-            code_ += typeLine;
-          }
-        }
-        code_ += "  }";  // close properties
-
-        if (&s != &parser_.structs_.vec.back()) {
-          code_ += "},";  // close type
+    std::string GenType(const Type &type) {      
+      switch (type.base_type) {
+      case BASE_TYPE_VECTOR: {
+        std::stringstream typeline;
+        typeline << "\"type\" : \"array\", ";
+        if (type.element == BASE_TYPE_STRUCT) {
+          typeline << "\"items\" : { " << GenTypeRef(type.struct_def->name) << " }";
         }
         else {
-          code_ += "}";  // close type
+          typeline << "\"items\" : { " << GenType(GenNativeType(type.element)) << " }";
         }
+        
+        return typeline.str();
       }
-      code_ += "},";  // close definitions
+      case BASE_TYPE_STRUCT: {
+        return GenTypeRef(type.struct_def->name);
+      }
+      case BASE_TYPE_UNION: {
+        std::stringstream unionTypes;
+        unionTypes << "\"anyOf\": [";
+        for(auto const& ut : type.enum_def->vals.vec) {
+          if (ut->union_type.base_type == BASE_TYPE_NONE) {
+            continue;
+          }
+          if (ut->union_type.base_type == BASE_TYPE_STRUCT) {
+            unionTypes << "{ " + GenTypeRef(ut->name) + " }";
+          }
 
-      // mark root type
-      code_ += "\"$ref\" : \"#/definitions/" + parser_.root_struct_def_->name + "\"";
-      code_ += "}";  // close schema root
-      const auto file_path = GeneratedFileName(path_, file_name_);
-      const auto final_code = code_.ToString();
-      return SaveFile(file_path.c_str(), final_code, false);
+          if (&ut != &type.enum_def->vals.vec.back()) {
+            unionTypes << ",";
+          }
+        }
+        unionTypes << "]";
+        return unionTypes.str();
+      }
+      case BASE_TYPE_UTYPE:   
+        return GenType("string");
+      default:
+        return GenType(GenNativeType(type.base_type));
+      }
     }
-  };
-}  // namespace jsons
 
-bool GenerateJsonSchema(const Parser &parser, const std::string &path, const std::string &file_name) {
-  jsons::JsonSchemaGenerator generator(parser, path, file_name);
-  return generator.generate();
-}
+    class JsonSchemaGenerator : public BaseGenerator {
+    private:
+      CodeWriter code_;
+    public:
+      JsonSchemaGenerator(const Parser& parser, const std::string& path, const std::string& file_name)
+        : BaseGenerator(parser, path, file_name, "", "") {
+      }
+
+      explicit JsonSchemaGenerator(const BaseGenerator& base_generator)
+        : BaseGenerator(base_generator) {
+      }
+
+      bool generate() override {
+        code_.Clear();
+        code_ += "{";
+        code_ += "\"$schema\": \"http://json-schema.org/draft-04/schema#\",";
+        code_ += "\"definitions\": {";
+
+        for (auto &s : parser_.structs_.vec) {
+          code_ += "";
+          code_ += "\"" + s->name + "\" : {";
+          code_ += "\"type\" : \"object\",";
+          std::stringstream comment;
+          for (auto commentLine : s->doc_comment) {
+            comment << commentLine;
+          }
+          code_ += "\"description\" : \"" + comment.str() + "\",";
+          code_ += "\"properties\" : {";
+
+          for (auto const &prop : s->fields.vec) {
+            std::stringstream typeLine;
+            typeLine << "  \""+prop->name+"\" : { " + GenType(prop->value.type) + " }";
+            
+            if (&prop != &s->fields.vec.back()) {
+              code_ += typeLine.str() + ",";
+            }
+            else {
+              code_ += typeLine.str();
+            }
+          }
+          code_ += "  }";  // close properties
+
+          if (&s != &parser_.structs_.vec.back()) {
+            code_ += "},";  // close type
+          }
+          else {
+            code_ += "}";  // close type
+          }
+
+          // todo
+          // insert required fields 
+          // "required" : [ "prop1", "prop2", ...]
+
+        }
+        code_ += "},";  // close definitions
+
+        // mark root type
+        code_ += "\"$ref\" : \"#/definitions/" + parser_.root_struct_def_->name + "\"";
+        
+        code_ += "}";  // close schema root
+        const auto file_path = GeneratedFileName(path_, file_name_);
+        const auto final_code = code_.ToString();
+        return SaveFile(file_path.c_str(), final_code, false);
+      }
+    };
+  }  // namespace jsons
+
+  bool GenerateJsonSchema(const Parser &parser, const std::string &path, const std::string &file_name) {
+    jsons::JsonSchemaGenerator generator(parser, path, file_name);
+    return generator.generate();
+  }
 }  // namespace flatbuffers
