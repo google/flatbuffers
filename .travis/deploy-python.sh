c23b2c2ea8d20dab3a64ac3d3a23af49f7ee6c03
Mike Holler <mike.holler@physiq.com>
2017-12-13 15:14:01 -0600

    Change mikeholler to PYPI_USERNAME

--- .travis/deploy-python.sh
+++ .travis/deploy-python.sh
@@ -5,7 +5,7 @@ PROD_REPOSITORY="https://upload.pypi.org/legacy/"
 TEST_REPOSITORY="https://test.pypi.org/legacy/"
 
 twine upload \
-    --username mikeholler \
+    --username "$PYPI_USERNAME" \
     --password "$PYPI_PASSWORD" \
     --repository-url "$PROD_REPOSITORY" \
     "$DIR/../python/dist/"*
