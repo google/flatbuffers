42de89fea50450592a5efef102a370cb646c126e
Uilian Ries <uilianries@gmail.com>
2018-01-13 18:53:38 -0200

    Build Conan package on OSX (#4590)
    - Added jobs to build Flatbuffers on OSX running on Travis
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ conan/travis/osx_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+python conan/build.py
