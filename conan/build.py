e37453db50dedd7d3746e8a2518745678f2ed6dc
Uilian Ries <uilianries@gmail.com>
2018-08-12 23:42:11 -0300

    Update Conan username (#4590)
    - Use google as default username
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/build.py
+++ conan/build.py
@@ -14,14 +14,17 @@ def set_appveyor_environment():
         os.environ["CONAN_BUILD_TYPES"] = os.getenv("Configuration").replace('"', '')
 
 if __name__ == "__main__":
-    username = os.getenv("CONAN_USERNAME", "aardappel")
+    login_username = os.getenv("CONAN_LOGIN_USERNAME", "aardappel")
+    username = os.getenv("CONAN_USERNAME", "google")
     upload = os.getenv("CONAN_UPLOAD", "https://api.bintray.com/conan/aardappel/flatbuffers")
     stable_branch_pattern = os.getenv("CONAN_STABLE_BRANCH_PATTERN", r"v\d+\.\d+\.\d+.*")
     test_folder = os.getenv("CPT_TEST_FOLDER", os.path.join("conan", "test_package"))
     upload_only_when_stable = os.getenv("CONAN_UPLOAD_ONLY_WHEN_STABLE", True)
     set_appveyor_environment()
 
-    builder = ConanMultiPackager(username=username, upload=upload,
+    builder = ConanMultiPackager(username=username,
+                                 login_username=login_username,
+                                 upload=upload,
                                  stable_branch_pattern=stable_branch_pattern,
                                  upload_only_when_stable=upload_only_when_stable,
                                  test_folder=test_folder)
