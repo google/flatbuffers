5ce974f445de248d49cf8d743a520c797ad8788b
Shivendra Agarwal <shiva.agarwal0@gmail.com>
2018-08-24 19:58:06 +0530

    another reordering

--- include/flatbuffers/flexbuffers.h
+++ include/flatbuffers/flexbuffers.h
@@ -367,9 +367,9 @@ class Reference {
   bool IsNumeric() const { return IsIntOrUint() || IsFloat(); }
   bool IsString() const { return type_ == FBT_STRING; }
   bool IsKey() const { return type_ == FBT_KEY; }
-  bool IsTypedVectorElementType() const { return flexbuffers::IsTypedVectorElementType(type_); }
   bool IsVector() const { return type_ == FBT_VECTOR || type_ == FBT_MAP; }
   bool IsTypedVector() const { return flexbuffers::IsTypedVector(type_); }
+  bool IsTypedVectorElementType() const { return flexbuffers::IsTypedVectorElementType(type_); }
   bool IsFixedTypedVector() const { return flexbuffers::IsFixedTypedVector(type_); }
   bool IsAnyVector() const { return (IsTypedVector() || IsFixedTypedVector() || IsVector());}
   bool IsMap() const { return type_ == FBT_MAP; }
