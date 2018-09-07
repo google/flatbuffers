6e6a452623d285078aae6c5495c4ddd46826b8a2
rw <me@rwinslow.com>
2018-09-06 19:56:18 -0700

    Add Rust to Appveyor config.
      RustTest.bat now aborts CI if the tests fail.

--- tests/RustTest.sh
+++ tests/RustTest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 set -ex
 #
 # Copyright 2018 Google Inc. All rights reserved.
@@ -16,7 +16,7 @@ set -ex
 # limitations under the License.
 
 cd ./rust_usage_test
-cargo test $1
+cargo test -- --quiet
 TEST_RESULT=$?
 if [[ $TEST_RESULT  == 0 ]]; then
     echo "OK: Rust tests passed."
