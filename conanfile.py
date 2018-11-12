daa1e24c76ffced0e3bd660341862b6bb8714455
Uilian Ries <uilianries@gmail.com>
2018-11-12 14:49:42 -0200

    Update Conan version Automatically (#5027)
    * #5026 Add FindFlatBuffers.cmake to Conan Package
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #5026 Build Flatbuffers on OSX 10
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #5026 Auto update Conan package version
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #5026 Update Conan docker images
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #5026 Update tag var on Appveyor
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #5026 Filter appveyor brach name
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -10,15 +10,15 @@ from conans import ConanFile, CMake, tools
 
 class FlatbuffersConan(ConanFile):
     name = "flatbuffers"
-    version = "1.10.0"
     license = "Apache-2.0"
     url = "https://github.com/google/flatbuffers"
     homepage = "http://google.github.io/flatbuffers/"
     author = "Wouter van Oortmerssen"
+    topics = ("conan", "flatbuffers", "serialization", "rpc", "json-parser")
     description = "Memory Efficient Serialization Library"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
     exports = "LICENSE.txt"
     exports_sources = ["CMake/*", "include/*", "src/*", "grpc/*", "CMakeLists.txt", "conan/CMakeLists.txt"]
@@ -57,6 +57,7 @@ class FlatbuffersConan(ConanFile):
         cmake = self.configure_cmake()
         cmake.install()
         self.copy(pattern="LICENSE.txt", dst="licenses")
+        self.copy(pattern="FindFlatBuffers.cmake", dst=os.path.join("lib", "cmake", "flatbuffers"), src="CMake")
         self.copy(pattern="flathash*", dst="bin", src="bin")
         self.copy(pattern="flatc*", dst="bin", src="bin")
         if self.settings.os == "Windows" and self.options.shared:
