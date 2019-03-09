aded82aad7f6d9f786be00424aa499fd584b9f9b
Bradlee Speice <bradlee.speice@gmail.com>
2019-03-09 10:49:50 -0600

    Add the rest of the `allow` attributes

--- tests/rust_usage_test/benches/flatbuffers_benchmarks.rs
+++ tests/rust_usage_test/benches/flatbuffers_benchmarks.rs
@@ -20,6 +20,7 @@ use bencher::Bencher;
 
 extern crate flatbuffers;
 
+#[allow(dead_code, unused_imports)]
 #[path = "../../monster_test_generated.rs"]
 mod monster_test_generated;
 pub use monster_test_generated::my_game;
--- tests/rust_usage_test/bin/monster_example.rs
+++ tests/rust_usage_test/bin/monster_example.rs
@@ -1,5 +1,6 @@
 extern crate flatbuffers;
 
+#[allow(dead_code, unused_imports)]
 #[path = "../../monster_test_generated.rs"]
 mod monster_test_generated;
 pub use monster_test_generated::my_game;
