69009bf637216e1573fff88a34f043cb49989681
Anthony Liot <anthony.liot@gmail.com>
2019-04-02 07:31:13 -0700

    use auto
    Signed-off-by: Anthony Liot <anthony.liot@gmail.com>

--- src/idl_gen_text.cpp
+++ src/idl_gen_text.cpp
@@ -266,11 +266,11 @@ static bool GenStruct(const StructDef &struct_def, const Table *table,
 bool GenerateTextFromTable(const Parser &parser, const void *table,
                            const std::string &table_name,
                            std::string *_text) {
-  StructDef *struct_def = parser.LookupStruct(table_name);
+  auto struct_def = parser.LookupStruct(table_name);
   if (struct_def == nullptr) {
     return false;
   }
-  std::string &text = *_text;
+  auto text = *_text;
   text.reserve(1024);               // Reduce amount of inevitable reallocs.
   auto root = static_cast<const Table*>(table);
   if (!GenStruct(*struct_def, root, 0, parser.opts, _text)) {
