e4740a31249b33a7a35c4176a04dbe87784f1af1
Alexey Geraskin <ageraskin@aligntech.com>
2019-07-17 15:07:09 +0300

    [C++] remove "= default" from constructor to support old compilers

--- tests/vector3d.h
+++ tests/vector3d.h
@@ -3,12 +3,11 @@
 
 namespace Native {
   struct Vector3D {
-  public:
     double x;
     double y;
     double z;
 
-    Vector3D() = default;
+    Vector3D() { x = 0; y = 0; z = 0; };
     Vector3D(double x, double y, double z) { this->x = x; this->y = y; this->z = z; }
   };
 }
