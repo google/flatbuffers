c1058a903bc908cc6d703512ed291e580bac09e0
Wes McKinney <wesm@users.noreply.github.com>
2019-08-19 19:49:37 -0500

    C++ IDL generation adds superfluous semicolon in GenTablePost, causing (#5483)
    -Wextra-semi-stmt warning when building with Clang 8.0.0. Update generated
    files for testing

--- tests/native_type_test_generated.h
+++ tests/native_type_test_generated.h
@@ -136,7 +136,7 @@ inline ApplicationDataT *ApplicationData::UnPack(const flatbuffers::resolver_fun
 inline void ApplicationData::UnPackTo(ApplicationDataT *_o, const flatbuffers::resolver_function_t *_resolver) const {
   (void)_o;
   (void)_resolver;
-  { auto _e = vectors(); if (_e) { _o->vectors.resize(_e->size()); for (flatbuffers::uoffset_t _i = 0; _i < _e->size(); _i++) { _o->vectors[_i] = flatbuffers::UnPack(*_e->Get(_i)); } } };
+  { auto _e = vectors(); if (_e) { _o->vectors.resize(_e->size()); for (flatbuffers::uoffset_t _i = 0; _i < _e->size(); _i++) { _o->vectors[_i] = flatbuffers::UnPack(*_e->Get(_i)); } } }
 }
 
 inline flatbuffers::Offset<ApplicationData> ApplicationData::Pack(flatbuffers::FlatBufferBuilder &_fbb, const ApplicationDataT* _o, const flatbuffers::rehasher_function_t *_rehasher) {
