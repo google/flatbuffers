29613226368be15add88f72cef5bb7481103dfc1
Wouter van Oortmerssen <aardappel@gmail.com>
2019-11-07 14:35:42 -0800

    Improved pull request & clang-format instructions.
    Change-Id: Ia439bcc61bac5788792244d10e573b1fba54b347

--- src/clang-format.sh
+++
@@ -1,2 +0,0 @@
-clang-format -i -style=file include/flatbuffers/* src/*.cpp tests/test.cpp samples/*.cpp grpc/src/compiler/schema_interface.h grpc/tests/*.cpp
-git checkout include/flatbuffers/reflection_generated.h
