3c964e10ab4f97e2a3602a8d0a8f4c402806ef89
Tiago Silva <3629062+tigrato@users.noreply.github.com>
2019-11-26 20:25:36 +0000

    [GO] Fix support for enums with underscores and Unions with imported members (#5600)
    * Fix Enum Stringer method when Enum has underscores
    
    Fixes #5481
    
    * Fix go package importing itself when Union has imported members.

--- tests/MyGame/Example/AnyUniqueAliases.go
+++ tests/MyGame/Example/AnyUniqueAliases.go
@@ -10,6 +10,36 @@ import (
 	MyGame__Example2 "MyGame/Example2"
 )
 
+type AnyUniqueAliases byte
+
+const (
+	AnyUniqueAliasesNONE AnyUniqueAliases = 0
+	AnyUniqueAliasesM    AnyUniqueAliases = 1
+	AnyUniqueAliasesTS   AnyUniqueAliases = 2
+	AnyUniqueAliasesM2   AnyUniqueAliases = 3
+)
+
+var EnumNamesAnyUniqueAliases = map[AnyUniqueAliases]string{
+	AnyUniqueAliasesNONE: "NONE",
+	AnyUniqueAliasesM:    "M",
+	AnyUniqueAliasesTS:   "TS",
+	AnyUniqueAliasesM2:   "M2",
+}
+
+var EnumValuesAnyUniqueAliases = map[string]AnyUniqueAliases{
+	"NONE": AnyUniqueAliasesNONE,
+	"M":    AnyUniqueAliasesM,
+	"TS":   AnyUniqueAliasesTS,
+	"M2":   AnyUniqueAliasesM2,
+}
+
+func (v AnyUniqueAliases) String() string {
+	if s, ok := EnumNamesAnyUniqueAliases[v]; ok {
+		return s
+	}
+	return "AnyUniqueAliases(" + strconv.FormatInt(int64(v), 10) + ")"
+}
+
 type AnyUniqueAliasesT struct {
 	Type AnyUniqueAliases
 	Value interface{}
@@ -43,34 +73,4 @@ func AnyUniqueAliasesUnPack(t AnyUniqueAliases, table flatbuffers.Table) *AnyUni
 		return &AnyUniqueAliasesT{ Type: AnyUniqueAliasesM2, Value: x.UnPack() }
 	}
 	return nil
-}
-
-type AnyUniqueAliases byte
-
-const (
-	AnyUniqueAliasesNONE AnyUniqueAliases = 0
-	AnyUniqueAliasesM    AnyUniqueAliases = 1
-	AnyUniqueAliasesTS   AnyUniqueAliases = 2
-	AnyUniqueAliasesM2   AnyUniqueAliases = 3
-)
-
-var EnumNamesAnyUniqueAliases = map[AnyUniqueAliases]string{
-	AnyUniqueAliasesNONE: "NONE",
-	AnyUniqueAliasesM:    "M",
-	AnyUniqueAliasesTS:   "TS",
-	AnyUniqueAliasesM2:   "M2",
-}
-
-var EnumValuesAnyUniqueAliases = map[string]AnyUniqueAliases{
-	"NONE": AnyUniqueAliasesNONE,
-	"M":    AnyUniqueAliasesM,
-	"TS":   AnyUniqueAliasesTS,
-	"M2":   AnyUniqueAliasesM2,
-}
-
-func (v AnyUniqueAliases) String() string {
-	if s, ok := EnumNamesAnyUniqueAliases[v]; ok {
-		return s
-	}
-	return "AnyUniqueAliases(" + strconv.FormatInt(int64(v), 10) + ")"
 }
