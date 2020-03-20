d8e5a7620094cc76e4ba094a825b6044ec0d2017
Olivier LDff <olivier.ldff@gmail.com>
2020-03-20 15:43:50 +0100

    [CMake] : Add precompiled header support with FLATBUFFERS_ENABLE_PCH
    FLATBUFFERS_ENABLE_PCH enable precompile headers support for 'flatbuffers' and 'flatc'. Default if OFF.
    You need to set it to ON to make it work. 'cmake -DFLATBUFFERS_ENABLE_PCH=ON ..'
    This will only work if CMake version is >= 3.16, because target_precompile_headers is used.
    If CMake doesn't have target_precompile_headers, then nothing will happen.
    This can speed up compilation time.
    Precompiled header file for 'flatbuffers' is 'pch.h'
    Precompiled header file for 'flatc' is 'flatc_pch.h'

---
+++ include/flatbuffers/pch.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FLATBUFFERS_PCH_H_
+#define FLATBUFFERS_PCH_H_
+
+// hash.h
+#include <cstdint>
+
+// hash.h
+#include <cstring>
+
+// idl_parser.cpp
+#include <algorithm>
+
+// idl_parser.cpp
+#include <list>
+
+// stl_emulation.h
+// idl_parser.cpp
+#include <string>
+
+// idl_parser.cpp
+#include <utility>
+#include <iomanip>
+
+// flexbuffers.h
+// idl.h
+#include <map>
+
+// idl.h
+// stl_emulation.h
+#include <memory>
+
+// stl_emulation.h
+#include <limits>
+
+// idl.h
+#include <stack>
+
+// stl_emulation.h
+#include <vector>
+
+// stl_emulation.h
+#include <type_traits>
+
+// flatbuffers
+#include "flatbuffers/util.h"
+
+#endif // FLATBUFFERS_PCH_H_
