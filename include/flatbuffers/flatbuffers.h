13687c4db447f7eef6d16485583cde6f81b89c63
stefan301 <32997632+stefan301@users.noreply.github.com>
2020-05-29 19:00:46 +0200

    Add GetStringView like GetString, GetCstring (#5937)
    * Added missing EndTable() call to VerifyObject()
    
    VerifyObject called VerifyTableStart() but not EndTable(). This made Verifier::VerifyComplexity() increase depth_ with each table, not with the depth of tables.
    
    https://groups.google.com/forum/#!topic/flatbuffers/OpxtW5UFAdg
    
    * Added Check to VerifyAlignment
    
    https://stackoverflow.com/questions/59376308/flatbuffers-verifier-returns-false-without-any-assertion-flatbuffers-debug-veri
    
    * Add GetStringView (Convenience function to get string_view from a String returning an empty string_view on null pointer) like GetString, GetCstring
    
    Co-authored-by: stefan301 <Stefan.Felkel@de.Zuken.com>

--- include/flatbuffers/flatbuffers.h
+++ include/flatbuffers/flatbuffers.h
@@ -581,6 +581,14 @@ static inline const char *GetCstring(const String *str) {
   return str ? str->c_str() : "";
 }
 
+#ifdef FLATBUFFERS_HAS_STRING_VIEW
+// Convenience function to get string_view from a String returning an empty
+// string_view on null pointer.
+static inline flatbuffers::string_view GetStringView(const String *str) {
+  return str ? str->string_view() : flatbuffers::string_view();
+}
+#endif // FLATBUFFERS_HAS_STRING_VIEW
+
 // Allocator interface. This is flatbuffers-specific and meant only for
 // `vector_downward` usage.
 class Allocator {
