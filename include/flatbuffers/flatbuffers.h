4acef90967004720c8343ece577452a2406eeb25
jdess <4703565+jdess@users.noreply.github.com>
2020-07-24 19:35:27 +0200

    [C++] Fix compiler error from deleted assignment operator (#6036) (#6047)
    * [C++] Fix compiler error from deleted assignment operator (#6036)
    
    The assignment operator of the `buf_` member is deleted, we cannot call it from the assignment operator of the `TableKeyComparator` struct.
    
    => Also delete the assignment operator of the `TableKeyComparator` struct (already private anyhow).
    
    * [C++] Fix compiler error from deleted assignment operator (#6036) - fix extraneous semicolon
    
    The assignment operator of the `buf_` member is deleted, we cannot call it from the assignment operator of the `TableKeyComparator` struct.
    
    => Also delete the assignment operator of the `TableKeyComparator` struct (already private anyhow).

--- include/flatbuffers/flatbuffers.h
+++ include/flatbuffers/flatbuffers.h
@@ -1940,10 +1940,7 @@ class FlatBufferBuilder {
     vector_downward &buf_;
 
    private:
-    TableKeyComparator &operator=(const TableKeyComparator &other) {
-      buf_ = other.buf_;
-      return *this;
-    }
+    FLATBUFFERS_DELETE_FUNC(TableKeyComparator &operator=(const TableKeyComparator &other))
   };
   /// @endcond
 
