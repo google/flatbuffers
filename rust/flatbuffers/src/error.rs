8665f99f5cafaf7d38bb55b46a39dccc2ba9699c
Kamil Rojewski <kamil.rojewski@gmail.com>
2020-10-05 16:57:53 +0200

    removed thiserror dependency

--- rust/flatbuffers/src/error.rs
+++ rust/flatbuffers/src/error.rs
@@ -1,7 +1,17 @@
-use thiserror::Error;
+use std::error::Error;
+use std::fmt;
 
-#[derive(Error, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 pub enum ConvertError {
-    #[error("invalid value in buffer")]
     InvalidValue,
 }
+
+impl fmt::Display for ConvertError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            ConvertError::InvalidValue => write!(f, "invalid value in buffer"),
+        }
+    }
+}
+
+impl Error for ConvertError {}
