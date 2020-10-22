aa608c876e895322f7bac3384870c12ce6573a89
Paulo Pinheiro <paulovap.os@gmail.com>
2020-10-22 14:53:43 +0200

    Add test for optional enums in Kotlin

--- tests/generate_code.sh
+++ tests/generate_code.sh
@@ -53,8 +53,7 @@ $TEST_NOINCL_FLAGS $TEST_CPP_FLAGS $TEST_CS_FLAGS $TEST_JS_TS_FLAGS -o namespace
 ../flatc --dart monster_extra.fbs
 
 # Generate optional scalar code for tests.
-../flatc --kotlin optional_scalars.fbs  # These ones have not added optional enum support.
-../flatc --rust --lobster optional_scalars2.fbs
+../flatc --kotlin --rust --lobster optional_scalars2.fbs
 ../flatc $TEST_NOINCL_FLAGS $TEST_CPP_FLAGS --cpp optional_scalars2.fbs
 
 # Generate the schema evolution tests
