e61c1143f6332d8930bc31899fe5a1cd9ab44f74
Charlie Yin <cyin@fortinet.com>
2020-11-02 17:44:15 -0800

    Change prefix size related library functions to conform with existing argument style

--- go/lib.go
+++ go/lib.go
@@ -13,11 +13,11 @@ func GetRootAs(buf []byte, offset UOffsetT, fb FlatBuffer) {
 }
 
 // GetSizePrefix reads the size from a size-prefixed flatbuffer
-func GetSizePrefix(buf []byte) uint32 {
-	return GetUint32(buf)
+func GetSizePrefix(buf []byte, offset UOffsetT) uint32 {
+	return GetUint32(buf[offset:])
 }
 
 // RemoveSizePrefix returns a slice of the original buffer starting from right after the prefixed size
-func RemoveSizePrefix(buf []byte) []byte {
-	return buf[sizePrefixLength:]
+func RemoveSizePrefix(buf []byte, offset UOffsetT) []byte {
+	return buf[offset+sizePrefixLength:]
 }
