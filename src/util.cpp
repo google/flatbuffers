de1f0342c86fc00955f6047937fac05e56b9b54c
Jason Lenz <Jason@lenzplace.org>
2020-11-12 12:12:32 -0600

    Remove _POSIX_C_SOURCE and _XOPEN_SOURCE definitions when compiling o… (#6205)
    * Remove _POSIX_C_SOURCE and _XOPEN_SOURCE definitions when compiling on OpenBSD
    
    * Only define _POSIX_C_SOURCE and _XOPEN_SOURCE for mingw/cygwin platforms
    
    * Only define POSIX statements for mingw/cygwin/qnx platforms

--- src/util.cpp
+++ src/util.cpp
@@ -17,8 +17,11 @@
 // clang-format off
 // Dont't remove `format off`, it prevent reordering of win-includes.
 
+#if defined(__MINGW32__) || defined(__MINGW64__) || defined(__CYGWIN__) || \
+    defined(__QNXNTO__)
 #  define _POSIX_C_SOURCE 200809L
 #  define _XOPEN_SOURCE 700L
+#endif
 
 #ifdef _WIN32
 #  ifndef WIN32_LEAN_AND_MEAN
