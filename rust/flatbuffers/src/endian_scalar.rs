b361742702bedcf28d86b2955f4f9c2d5d377da2
Casper Neo <cneo@google.com>
2021-01-09 09:43:38 -0500

    missing one endian conversion

--- rust/flatbuffers/src/endian_scalar.rs
+++ rust/flatbuffers/src/endian_scalar.rs
@@ -150,6 +150,7 @@ pub fn byte_swap_f64(x: f64) -> f64 {
 /// endian conversion, if necessary.
 #[inline]
 pub fn emplace_scalar<T: EndianScalar>(s: &mut [u8], x: T) {
+    let x_le = x.to_little_endian();
     unsafe {
         core::ptr::copy_nonoverlapping(
             (&x) as *const T as *const u8,
