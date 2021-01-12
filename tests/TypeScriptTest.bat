b99ea497468ca755775296382dd093a3f7e1df7e
Björn Harrtell <bjorn@wololo.org>
2021-01-12 20:47:27 +0100

    Want to see the output

--- tests/TypeScriptTest.bat
+++ tests/TypeScriptTest.bat
@@ -1,4 +1,3 @@
-@echo off
 npm install
 ../flatc.exe --ts --gen-name-strings --gen-mutable --gen-object-api -o ts -I include_test monster_test.fbs
 ../flatc.exe --gen-object-api -b -I include_test monster_test.fbs unicode_test.json
