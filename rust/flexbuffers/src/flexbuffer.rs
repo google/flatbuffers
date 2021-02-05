25527c3b65167b0e8cc6a665262e0531311acb41
Colin Kirkpatrick <colindjk@gmail.com>
2021-02-05 00:26:54 -0500

    Added proper &[u8] implementation

--- rust/flexbuffers/src/flexbuffer.rs
+++ rust/flexbuffers/src/flexbuffer.rs
@@ -1,6 +1,4 @@
-use std::iter::{Iterator};
-
-use std::ops::{RangeBounds, Bound, Index};
+use std::ops::{RangeBounds, Rem, Bound};
 use std::slice::SliceIndex;
 use std::fmt::Debug;
 
@@ -35,19 +33,47 @@ impl FlexBuffer for &[u8] {
     /// Gets the length of the current buffer.
     #[inline]
     fn len(&self) -> usize {
-        self.len()
+        <[u8]>::len(self)
     }
 
     /// Retrieves a slice of memory based on RangeBounds.
     fn slice(&self, range: impl RangeBounds<usize>) -> Self {
-        self.slice(range)
+        // Taken from bytes::Bytes `slice` implementation.
+        let len = self.len();
+
+        let begin = match range.start_bound() {
+            Bound::Included(&n) => n,
+            Bound::Excluded(&n) => n + 1,
+            Bound::Unbounded => 0,
+        };
+
+        let end = match range.end_bound() {
+            Bound::Included(&n) => n.checked_add(1).expect("out of range"),
+            Bound::Excluded(&n) => n,
+            Bound::Unbounded => len,
+        };
+
+        assert!(
+            begin <= end,
+            "range start must not be greater than end: {:?} <= {:?}",
+            begin,
+            end,
+        );
+        assert!(
+            end <= len,
+            "range end out of bounds: {:?} <= {:?}",
+            end,
+            len,
+        );
+
+        &self[begin..end]
     }
 
     /// Similar to [T]::get, get a value at an index.
     fn get<I>(&self, index: I) -> Option<&<I as SliceIndex<[u8]>>::Output>
         where I: SliceIndex<[u8]>
     {
-        self.get(index)
+        <[u8]>::get(self, index)
     }
 
     /// Returns true if the flexbuffer is aligned to 8 bytes. This guarantees, for valid
@@ -55,11 +81,11 @@ impl FlexBuffer for &[u8] {
     /// e.g. with `get_f64s` or `get_i16s`.
     #[inline]
     fn is_aligned(&self) -> bool {
-        self.is_aligned()
+        (self.as_ptr() as usize).rem(8) == 0
     }
 
     fn align_to<L: ReadLE>(&self) -> (&[u8], &[L], &[u8]) {
-        self.align_to::<L>()
+        unsafe { <[u8]>::align_to::<L>(self) }
     }
 }
 
