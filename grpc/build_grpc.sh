6c967696d8a86f39af3ca4db3156654f76cca5a4
Jeong YunWon <jeong@youknowone.org>
2021-03-03 06:22:23 +0900

    grpc 1.36.0

--- grpc/build_grpc.sh
+++ grpc/build_grpc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-grpc_1_15_1_githash=1a60e6971f428323245a930031ad267bb3142ba4
+grpc_1_36_0_githash=736e3758351ced3cd842bad3ba4e2540f01bbc48
 
 function build_grpc () {
   git clone https://github.com/grpc/grpc.git google/grpc
   cd google/grpc
-  git checkout ${grpc_1_15_1_githash}
+  git checkout ${grpc_1_36_0_githash}
   git submodule update --init
+  cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=`pwd`/install
   make
-  make install prefix=`pwd`/install
+  make install
   if [ ! -f ${GRPC_INSTALL_PATH}/lib/libgrpc++_unsecure.so.1 ]; then
     ln -s ${GRPC_INSTALL_PATH}/lib/libgrpc++_unsecure.so.6 ${GRPC_INSTALL_PATH}/lib/libgrpc++_unsecure.so.1
   fi
