e81a91c0ec26fc0dcf81ee403fb40f0eb75c270d
Gyusun Yeom <omniavinco@gmail.com>
2021-03-25 18:01:03 +0900

    Apply workaround about boringssl build error

--- grpc/build_grpc.sh
+++ grpc/build_grpc.sh
@@ -7,11 +7,14 @@ function build_grpc () {
   cd google/grpc
   git checkout ${grpc_1_36_0_githash}
   git submodule update --init
+  # Apply boringssl build patch
+  cd third_party/boringssl-with-bazel
+  git apply ../../../../grpc/boringssl.patch
+  cd ../..
   mkdir ../grpc_build
   cd ../grpc_build
   cmake ../grpc -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=`pwd`/../grpc/install
-  make
-  make install
+  cmake --build . --target install ${JOBS:+-j$JOBS}
   if [ ! -f ${GRPC_INSTALL_PATH}/lib/libgrpc++_unsecure.so.1 ]; then
     ln -s ${GRPC_INSTALL_PATH}/lib/libgrpc++_unsecure.so.6 ${GRPC_INSTALL_PATH}/lib/libgrpc++_unsecure.so.1
   fi
