fbcb3c4236c61db3622ebe8b5a550925720ec8f6
mustiikhalil <mustii@mmk.one>
2021-05-20 21:34:17 +0300

    [TS/JS] Updates the grpc (#6654)
    * Updates go lang support to allow other languages to communicate with it
    
    * Update js grpc lib to use grpc-js
    
    Reformat code

--- grpc/examples/ts/greeter/src/client.ts
+++ grpc/examples/ts/greeter/src/client.ts
@@ -1,22 +1,22 @@
-import grpc from 'grpc';
+import * as grpc from '@grpc/grpc-js';
+import * as flatbuffers from 'flatbuffers';
 import { HelloReply } from './models/hello-reply';
 import { HelloRequest } from './models/hello-request';
 import { GreeterClient } from './greeter_grpc';
-import { flatbuffers } from 'flatbuffers';
 
-async function main(PORT: Number, name: String) {
-    const _server = new GreeterClient(`localhost:${PORT}`, grpc.credentials.createInsecure());
+async function main(PORT: Number, name: string) {
+    const client = new GreeterClient(`localhost:${PORT}`, grpc.credentials.createInsecure());
     const builder = new flatbuffers.Builder();
     const offset = builder.createString(name);
     const root = HelloRequest.createHelloRequest(builder, offset);
     builder.finish(root);
     const buffer = HelloRequest.getRootAsHelloRequest(new flatbuffers.ByteBuffer(builder.asUint8Array()));
 
-    _server.SayHello(buffer, (err, response) => {
+    client.SayHello(buffer, (err, response) => {
         console.log(response.message());
     });
 
-    const data = _server.SayManyHellos(buffer, null);
+    const data = client.SayManyHellos(buffer, null);
 
     data.on('data', (data) => {
         console.log(data.message());
@@ -25,7 +25,7 @@ async function main(PORT: Number, name: String) {
 
 const args = process.argv.slice(2)
 const PORT = Number(args[0]);
-const name = String(args[1] ?? "flatbuffers");
+const name: string = args[1] ?? "flatbuffers";
 
 if (PORT) {
     main(PORT, name);
