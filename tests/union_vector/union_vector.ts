f9106b687184e78b5a916c8ca690cc83b8085719
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2021-06-01 18:09:55 +0200

    [TS] Generate entry point module (#6674)

---
+++ tests/union_vector/union_vector.ts
@@ -0,0 +1,5 @@
+export { Attacker, AttackerT } from './attacker';
+export { BookReader, BookReaderT } from './book-reader';
+export { Character, unionToCharacter, unionListToCharacter } from './character';
+export { Movie, MovieT } from './movie';
+export { Rapunzel, RapunzelT } from './rapunzel';
