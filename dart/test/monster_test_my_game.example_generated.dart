fd0b112a6ad1ee3b90120de2931ac696621a85e8
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2021-07-20 16:54:48 +0200

    Dart - make writeString() argument non-nullable (#6737)

--- dart/test/monster_test_my_game.example_generated.dart
+++ dart/test/monster_test_my_game.example_generated.dart
@@ -846,7 +846,8 @@ class StatT {
       this.count = 0});
 
   int pack(fb.Builder fbBuilder) {
-    final int? idOffset = fbBuilder.writeString(id);
+    final int? idOffset = id == null ? null
+        : fbBuilder.writeString(id!);
     fbBuilder.startTable(3);
     fbBuilder.addOffset(0, idOffset);
     fbBuilder.addInt64(1, val);
@@ -912,7 +913,8 @@ class StatObjectBuilder extends fb.ObjectBuilder {
   /// Finish building, and store into the [fbBuilder].
   @override
   int finish(fb.Builder fbBuilder) {
-    final int? idOffset = fbBuilder.writeString(_id);
+    final int? idOffset = _id == null ? null
+        : fbBuilder.writeString(_id!);
     fbBuilder.startTable(3);
     fbBuilder.addOffset(0, idOffset);
     fbBuilder.addInt64(1, _val);
@@ -1285,7 +1287,8 @@ class MonsterT {
       this.scalarKeySortedTables});
 
   int pack(fb.Builder fbBuilder) {
-    final int? nameOffset = fbBuilder.writeString(name);
+    final int? nameOffset = name == null ? null
+        : fbBuilder.writeString(name!);
     final int? inventoryOffset = inventory == null ? null
         : fbBuilder.writeListUint8(inventory!);
     final int? testOffset = test?.pack(fbBuilder);
@@ -1295,7 +1298,7 @@ class MonsterT {
       test4Offset = fbBuilder.endStructVector(test4!.length);
     }
     final int? testarrayofstringOffset = testarrayofstring == null ? null
-        : fbBuilder.writeList(testarrayofstring!.map((b) => fbBuilder.writeString(b)!).toList());
+        : fbBuilder.writeList(testarrayofstring!.map(fbBuilder.writeString).toList());
     final int? testarrayoftablesOffset = testarrayoftables == null ? null
         : fbBuilder.writeList(testarrayoftables!.map((b) => b.pack(fbBuilder)).toList());
     final int? enemyOffset = enemy?.pack(fbBuilder);
@@ -1305,7 +1308,7 @@ class MonsterT {
     final int? testarrayofboolsOffset = testarrayofbools == null ? null
         : fbBuilder.writeListBool(testarrayofbools!);
     final int? testarrayofstring2Offset = testarrayofstring2 == null ? null
-        : fbBuilder.writeList(testarrayofstring2!.map((b) => fbBuilder.writeString(b)!).toList());
+        : fbBuilder.writeList(testarrayofstring2!.map(fbBuilder.writeString).toList());
     int? testarrayofsortedstructOffset = null;
     if (testarrayofsortedstruct != null) {
       testarrayofsortedstruct!.forEach((e) => e.pack(fbBuilder));
@@ -1784,14 +1787,15 @@ class MonsterObjectBuilder extends fb.ObjectBuilder {
   /// Finish building, and store into the [fbBuilder].
   @override
   int finish(fb.Builder fbBuilder) {
-    final int? nameOffset = fbBuilder.writeString(_name);
+    final int? nameOffset = _name == null ? null
+        : fbBuilder.writeString(_name!);
     final int? inventoryOffset = _inventory == null ? null
         : fbBuilder.writeListUint8(_inventory!);
     final int? testOffset = _test?.getOrCreateOffset(fbBuilder);
     final int? test4Offset = _test4 == null ? null
         : fbBuilder.writeListOfStructs(_test4!);
     final int? testarrayofstringOffset = _testarrayofstring == null ? null
-        : fbBuilder.writeList(_testarrayofstring!.map((b) => fbBuilder.writeString(b)!).toList());
+        : fbBuilder.writeList(_testarrayofstring!.map(fbBuilder.writeString).toList());
     final int? testarrayoftablesOffset = _testarrayoftables == null ? null
         : fbBuilder.writeList(_testarrayoftables!.map((b) => b.getOrCreateOffset(fbBuilder)).toList());
     final int? enemyOffset = _enemy?.getOrCreateOffset(fbBuilder);
@@ -1801,7 +1805,7 @@ class MonsterObjectBuilder extends fb.ObjectBuilder {
     final int? testarrayofboolsOffset = _testarrayofbools == null ? null
         : fbBuilder.writeListBool(_testarrayofbools!);
     final int? testarrayofstring2Offset = _testarrayofstring2 == null ? null
-        : fbBuilder.writeList(_testarrayofstring2!.map((b) => fbBuilder.writeString(b)!).toList());
+        : fbBuilder.writeList(_testarrayofstring2!.map(fbBuilder.writeString).toList());
     final int? testarrayofsortedstructOffset = _testarrayofsortedstruct == null ? null
         : fbBuilder.writeListOfStructs(_testarrayofsortedstruct!);
     final int? flexOffset = _flex == null ? null
