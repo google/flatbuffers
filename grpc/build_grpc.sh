c694e9f8cf34a359ffe83c5e7adcd6156adf986c
Esun Kim <veblush@google.com>
2021-07-23 11:11:41 -0700

    Upgrade gRPC to 1.39.0

--- grpc/build_grpc.sh
+++ grpc/build_grpc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-grpc_1_36_0_githash=736e3758351ced3cd842bad3ba4e2540f01bbc48
+grpc_1_39_0_githash=58602e20a3f3e48f24a4114c757099b25b947f7b
 
 function build_grpc () {
   git clone https://github.com/grpc/grpc.git google/grpc
   cd google/grpc
-  git checkout ${grpc_1_36_0_githash}
+  git checkout ${grpc_1_39_0_githash}
   git submodule update --init
   # Apply boringssl build patch
   cd third_party/boringssl-with-bazel
