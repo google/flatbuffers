65667b1b72fbbb1e0eec59e235d6c989f274164b
Casper Neo <cneo@google.com>
2021-09-13 16:22:23 -0400

    Deleted a bunch of generated files, not all were regenerated

--- tests/union_vector/union_vector.js
+++
@@ -1,5 +0,0 @@
-export { Attacker, AttackerT } from './attacker';
-export { BookReader, BookReaderT } from './book-reader';
-export { Character, unionToCharacter, unionListToCharacter } from './character';
-export { Movie, MovieT } from './movie';
-export { Rapunzel, RapunzelT } from './rapunzel';
