33fb3e05a04b5b67a7f95273ce6dd47cebe2cceb
Lu Wang <luwa@google.com>
2021-10-05 17:21:18 -0700

    Update the generated example code

--- grpc/examples/python/greeter/models/HelloRequest.py
+++ grpc/examples/python/greeter/models/HelloRequest.py
@@ -10,16 +10,16 @@ class HelloRequest(object):
     __slots__ = ['_tab']
 
     @classmethod
-    def GetRootAs(cls, buf, offset=0):
+    def GetRootAsHelloRequest(cls, buf, offset=0):
         n = flatbuffers.encode.Get(flatbuffers.packer.uoffset, buf, offset)
         x = HelloRequest()
         x.Init(buf, n + offset)
         return x
 
     @classmethod
-    def GetRootAsHelloRequest(cls, buf, offset=0):
-        """This method is deprecated. Please switch to GetRootAs."""
-        return cls.GetRootAs(buf, offset)
+    def GetRootAs(cls, buf, offset=0):
+        return cls.GetRootAsHelloRequest(buf, offset)
+
     # HelloRequest
     def Init(self, buf, pos):
         self._tab = flatbuffers.table.Table(buf, pos)
@@ -31,15 +31,12 @@ class HelloRequest(object):
             return self._tab.String(o + self._tab.Pos)
         return None
 
-def Start(builder): builder.StartObject(1)
-def HelloRequestStart(builder):
-    """This method is deprecated. Please switch to Start."""
-    return Start(builder)
-def AddName(builder, name): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(name), 0)
-def HelloRequestAddName(builder, name):
-    """This method is deprecated. Please switch to AddName."""
-    return AddName(builder, name)
-def End(builder): return builder.EndObject()
-def HelloRequestEnd(builder):
-    """This method is deprecated. Please switch to End."""
-    return End(builder)
+def HelloRequestStart(builder): builder.StartObject(1)
+def Start(builder):
+    return HelloRequestStart(builder)
+def HelloRequestAddName(builder, name): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(name), 0)
+def AddName(builder, name):
+    return HelloRequestAddName(builder, name)
+def HelloRequestEnd(builder): return builder.EndObject()
+def End(builder):
+    return HelloRequestEnd(builder)
