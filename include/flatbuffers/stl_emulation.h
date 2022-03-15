30a1cdf7502c84a9fc17ea1ea830cd968e6a3b0d
David E. Schneider <dschneider@tableau.com>
2022-03-14 21:10:04 -0700

    Make inclusion of header <optional> opt-in

--- include/flatbuffers/stl_emulation.h
+++ include/flatbuffers/stl_emulation.h
@@ -28,17 +28,14 @@
 
 // Detect C++17 compatible compiler.
 // __cplusplus >= 201703L - a compiler has support of 'static inline' variables.
-#if !defined(FLATBUFFERS_DONT_USE_STD_OPTIONAL) \
-    && ( \
-      defined(FLATBUFFERS_USE_STD_OPTIONAL) \
-      || (defined(__cplusplus) && __cplusplus >= 201703L) \
-      || (defined(_MSVC_LANG) &&  _MSVC_LANG >= 201703L) \
-    )
+#if defined(FLATBUFFERS_USE_STD_OPTIONAL) && ( \
+    (defined(__cplusplus) && __cplusplus >= 201703L) \
+    || (defined(_MSVC_LANG) &&  _MSVC_LANG >= 201703L))
   #include <optional>
   #ifndef FLATBUFFERS_USE_STD_OPTIONAL
     #define FLATBUFFERS_USE_STD_OPTIONAL
   #endif
-#endif // !defined(FLATBUFFERS_DONT_USE_STD_OPTIONAL) ...
+#endif // defined(FLATBUFFERS_USE_STD_OPTIONAL) && ...
 
 // The __cpp_lib_span is the predefined feature macro.
 #if defined(FLATBUFFERS_USE_STD_SPAN)
