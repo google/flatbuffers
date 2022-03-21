4168774cf320999f58ac4c70d63c826a5d9afaa4
Stefan F <32997632+stefan301@users.noreply.github.com>
2022-03-21 23:08:14 +0100

    Typo in flatc options (warning-as-errors instead of warnings-as-errors) (#7180)

--- src/flatc.cpp
+++ src/flatc.cpp
@@ -205,7 +205,7 @@ const static FlatCOption options[] = {
     "Used with \"binary\" and \"json\" options, it generates data using "
     "schema-less FlexBuffers." },
   { "", "no-warnings", "", "Inhibit all warnings messages." },
-  { "", "warning-as-errors", "", "Treat all warnings as errors." },
+  { "", "warnings-as-errors", "", "Treat all warnings as errors." },
   { "", "cs-global-alias", "",
     "Prepend \"global::\" to all user generated csharp classes and "
     "structs." },
