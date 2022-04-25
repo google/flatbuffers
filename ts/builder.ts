7654e302093cf63a467f8fd0bdfcd7c016f285a0
sssooonnnggg <sssooonnnggg111@gmail.com>
2022-04-25 18:19:53 +0800

    [TS] fix incorrect reverse when writting array of structs

--- ts/builder.ts
+++ ts/builder.ts
@@ -617,7 +617,7 @@ export class Builder {
   
     createStructOffsetList(list: string[] | any[], startFunc: (builder: Builder, length: number) => void): Offset {
       startFunc(this, list.length);
-      this.createObjectOffsetList(list);
+      this.createObjectOffsetList(list.slice().reverse());
       return this.endVector();
     }
   }
