7d2feec3d7fbf5d5983b13f4d0e4949569524094
Chris Langhans <chris@langhans.dev>
2022-07-12 10:22:18 +0200

    CMake find_package fixes (#7323)
    Rename FlatBuffersTargets to match CMake project name

--- CMake/FlatBuffersConfig.cmake
+++ CMake/FlatBuffersConfig.cmake
@@ -1,4 +1,3 @@
-include("${CMAKE_CURRENT_LIST_DIR}/FlatbuffersTargets.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/FlatBuffersTargets.cmake" OPTIONAL)
 include("${CMAKE_CURRENT_LIST_DIR}/FlatcTargets.cmake" OPTIONAL)
-include("${CMAKE_CURRENT_LIST_DIR}/FlatbuffersSharedTargets.cmake" OPTIONAL)
-
+include("${CMAKE_CURRENT_LIST_DIR}/FlatBuffersSharedTargets.cmake" OPTIONAL)
