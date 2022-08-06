22880dd4b40fee508d1a6db82dbce24618b9296d
Chris Langhans <chris@langhans.dev>
2022-08-06 07:19:17 +0200

    (#7323) Rename CMake files according to project name (#7378)
    * CMake find_package fixes (#7323)
    
    Rename FlatbuffersConfigVersion.cmake to match CMake project name
    
    * CMake find_package fixes (#7323)
    
    Rename FlatBuffersTargets to match CMake project name

---
+++ CMake/FlatBuffersConfig.cmake
@@ -0,0 +1,3 @@
+include("${CMAKE_CURRENT_LIST_DIR}/FlatBuffersTargets.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/FlatcTargets.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/FlatBuffersSharedTargets.cmake" OPTIONAL)
