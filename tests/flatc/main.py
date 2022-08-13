46cbe507233a2877036dda8636f86aa5c6de628e
Derek Bailey <derekbailey@google.com>
2022-08-12 20:58:39 -0700

    Add tests for ts flatc

--- tests/flatc/main.py
+++ tests/flatc/main.py
@@ -4,8 +4,10 @@ import sys;
 
 from flatc_test import run_all
 from flatc_cpp_tests import CppTests
+from flatc_ts_tests import TsTests
 
 passing, failing = run_all(CppTests)
+passing, failing = run_all(TsTests)
 
 if failing > 0:
   sys.exit(1)
