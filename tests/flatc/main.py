97a111a074dfac173ed552a6371d9f6ef6d18649
Derek Bailey <derekbailey@google.com>
2022-08-13 17:30:51 -0700

    Add flatc python tests to CI (#7437)

--- tests/flatc/main.py
+++ tests/flatc/main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import sys;
+
 from flatc_test import run_all
 from flatc_cpp_tests import CppTests
 
-run_all(CppTests)
+passing, failing = run_all(CppTests)
+
+if failing > 0:
+  sys.exit(1)
