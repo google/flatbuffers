4f788f6d3e8b25ac541d28606c37d2605a0716b1
Derek Bailey <derekbailey@google.com>
2022-08-24 10:36:11 -0700

    Disable RTTI and rework use in idl_gen_ts.cpp (#7474)

--- src/idl_gen_ts.cpp
+++ src/idl_gen_ts.cpp
@@ -38,6 +38,12 @@ struct ImportDefinition {
 };
 
 enum AnnotationType { kParam = 0, kType = 1, kReturns = 2 };
+
+template<typename T> bool SupportsObjectAPI() { return false; }
+
+// Structs can have Object API support.
+template<> bool SupportsObjectAPI<StructDef>() { return true; }
+
 }  // namespace
 
 namespace ts {
@@ -712,9 +718,9 @@ class TsGenerator : public BaseGenerator {
     return symbols_expression;
   }
 
-  template<typename DefintionT>
+  template<typename DefinitionT>
   ImportDefinition AddImport(import_set &imports, const Definition &dependent,
-                             const DefintionT &dependency) {
+                             const DefinitionT &dependency) {
     // The unique name of the dependency, fully qualified in its namespace.
     const std::string unique_name = GetTypeName(
         dependency, /*object_api = */ false, /*force_ns_wrap=*/true);
@@ -748,8 +754,9 @@ class TsGenerator : public BaseGenerator {
                 // Strip the leading //
                 .substr(2);
         flat_file_import_declarations_[file][import_name] = name;
+
         if (parser_.opts.generate_object_based_api &&
-            typeid(dependency) == typeid(StructDef)) {
+            SupportsObjectAPI<DefinitionT>()) {
           flat_file_import_declarations_[file][import_name + "T"] = object_name;
         }
       }
