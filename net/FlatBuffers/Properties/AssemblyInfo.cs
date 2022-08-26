adee51b28804d2a03670a2f311d07d5b247384d5
Derek Bailey <dbaileychess@gmail.com>
2022-08-26 15:56:20 -0700

    Define nuget package

--- net/FlatBuffers/Properties/AssemblyInfo.cs
+++ net/FlatBuffers/Properties/AssemblyInfo.cs
@@ -22,11 +22,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FlatBuffers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Memory Efficient Serialization Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Google")]
 [assembly: AssemblyProduct("FlatBuffers")]
-[assembly: AssemblyCopyright("Copyright (c) 2015 Google Inc")]
+[assembly: AssemblyCopyright("Copyright (c) 2022 Google Inc")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.7.0")]
+[assembly: AssemblyFileVersion("2.0.7.0")]
