06c5c7ed0bd987a918cf88caafb094f22cdd1721
Derek Bailey <derekbailey@google.com>
2022-08-29 20:43:36 -0700

    FlatBuffers Version 2.0.8 (#7492)

--- tests/MyGame/Example/Monster.java
+++ tests/MyGame/Example/Monster.java
@@ -12,7 +12,7 @@ import com.google.flatbuffers.*;
  */
 @SuppressWarnings("unused")
 public final class Monster extends Table {
-  public static void ValidateVersion() { Constants.FLATBUFFERS_2_0_0(); }
+  public static void ValidateVersion() { Constants.FLATBUFFERS_2_0_8(); }
   public static Monster getRootAsMonster(ByteBuffer _bb) { return getRootAsMonster(_bb, new Monster()); }
   public static Monster getRootAsMonster(ByteBuffer _bb, Monster obj) { _bb.order(ByteOrder.LITTLE_ENDIAN); return (obj.__assign(_bb.getInt(_bb.position()) + _bb.position(), _bb)); }
   public static boolean MonsterBufferHasIdentifier(ByteBuffer _bb) { return __has_identifier(_bb, "MONS"); }
