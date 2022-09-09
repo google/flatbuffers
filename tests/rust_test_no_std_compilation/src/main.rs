d750de0ac637720952a9e29d40c3be1db12afb7a
Juliette Pretot <julsh@google.com>
2022-09-09 10:52:24 +0000

    simplify nostd compile test so it works on stable rust

--- tests/rust_test_no_std_compilation/src/main.rs
+++ tests/rust_test_no_std_compilation/src/main.rs
@@ -1,29 +1,18 @@
 #![no_std]
 #![no_main]
-#![feature(alloc_error_handler)]
-#![feature(core_c_str)]
-#![feature(core_ffi_c)]
 
-/// Minimal no_std binary used to test that flatbuffers compiles in a no_std
-/// enviroment. Based on: https://blog.dbrgn.ch/2019/12/24/testing-for-no-std-compatibility/
-use core::panic::PanicInfo;
-use linked_list_allocator::LockedHeap;
-
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+extern crate wee_alloc;
 
 // Include flatbuffers purely to check that it compiles in a no_std binary
 #[allow(unused_imports)]
 use flatbuffers;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[alloc_error_handler]
-fn out_of_memory(layout: ::core::alloc::Layout) -> ! {
-    panic!("Error allocating memory: {:#?}", layout);
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
 
 #[no_mangle]
