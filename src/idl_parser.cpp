ddbe8032723a3e991eeb82bf0887eabf5e932a41
Can Oğuz <can.oguz@mediaz.io>
2022-11-22 12:13:19 +0300

    added missing preprocessor blocks

--- src/idl_parser.cpp
+++ src/idl_parser.cpp
@@ -1517,7 +1517,9 @@ CheckedError Parser::ParseTableDelimiters(size_t &fieldn,
   ParseDepthGuard depth_guard(this);
   ECHECK(depth_guard.Check());
 
+#if defined(MZ_CUSTOM_FLATBUFFERS) && MZ_CUSTOM_FLATBUFFERS  // clang-format off
   int32_t lastFieldCount = int32_t(field_stack_.size());
+#endif // defined(MZ_CUSTOM_FLATBUFFERS) && MZ_CUSTOM_FLATBUFFERS // clang-format on
 
   size_t fieldn_outer = 0;
   auto err = ParseTableDelimiters(
@@ -4495,7 +4497,7 @@ std::string Parser::ConformTo(const Parser &base) {
   return "";
 }
 
-// clang-format off
+#if defined(MZ_CUSTOM_FLATBUFFERS) && MZ_CUSTOM_FLATBUFFERS  // clang-format off
 Parser::type_lookup *Parser::LookupPrimitiveType(std::string const &name) {
   static type_lookup lookup[] = {
     { "f32", BASE_TYPE_FLOAT, BASE_TYPE_NONE },
@@ -4703,5 +4705,6 @@ bool Parser::CompleteMissingField(FieldDef* absent_field, const StructDef &struc
 
   return found;
 }
+#endif // defined(MZ_CUSTOM_FLATBUFFERS) && MZ_CUSTOM_FLATBUFFERS  // clang-format on
 
 }  // namespace flatbuffers
