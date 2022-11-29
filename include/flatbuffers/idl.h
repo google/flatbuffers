f9d39803aa60a58ddb99e85e903a3d9e7852bef8
sssooonnnggg <sssooonnnggg111@gmail.com>
2022-11-29 12:59:53 +0800

    chore: emit more reasonable error message when using incomplete type in struct (#7678)
    Co-authored-by: Derek Bailey <derekbailey@google.com>

--- include/flatbuffers/idl.h
+++ include/flatbuffers/idl.h
@@ -473,6 +473,10 @@ inline bool IsStruct(const Type &type) {
   return type.base_type == BASE_TYPE_STRUCT && type.struct_def->fixed;
 }
 
+inline bool IsIncompleteStruct(const Type &type) {
+  return type.base_type == BASE_TYPE_STRUCT && type.struct_def->predecl;
+}
+
 inline bool IsTable(const Type &type) {
   return type.base_type == BASE_TYPE_STRUCT && !type.struct_def->fixed;
 }
