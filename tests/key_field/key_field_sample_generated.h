b41084315acfad6030c239057619bdee660856e7
Wen Sun <sunwen@google.com>
2022-12-02 08:14:44 +0000

    address error

--- tests/key_field/key_field_sample_generated.h
+++ tests/key_field/key_field_sample_generated.h
@@ -47,12 +47,12 @@ FLATBUFFERS_MANUALLY_ALIGNED_STRUCT(1) Baz FLATBUFFERS_FINAL_CLASS {
   bool KeyCompareLessThan(const Baz * const o) const {
     return KeyCompareWithValue(o->a()) < 0;
   }
-  int KeyCompareWithValue(const flatbuffers::Array<uint8_t, 4> *_a) const {
-    curr_a = a();
+  int KeyCompareWithValue(const flatbuffers::Array<uint8_t, 4> *_a) const { 
+    auto curr_a = a();
     for (auto i = 0; i < a()->size(); i++) {
       const auto a_l = curr_a->Get(i);
       const auto a_r = _a->Get(i);
-      if(a_l != a_r)
+      if(a_l != a_r) 
         return static_cast<int>(a_l > a_r) - static_cast<int>(a_l < a_r);
     }
     return 0;
@@ -100,12 +100,12 @@ FLATBUFFERS_MANUALLY_ALIGNED_STRUCT(4) Bar FLATBUFFERS_FINAL_CLASS {
   bool KeyCompareLessThan(const Bar * const o) const {
     return KeyCompareWithValue(o->a()) < 0;
   }
-  int KeyCompareWithValue(const flatbuffers::Array<float, 3> *_a) const {
-    curr_a = a();
+  int KeyCompareWithValue(const flatbuffers::Array<float, 3> *_a) const { 
+    auto curr_a = a();
     for (auto i = 0; i < a()->size(); i++) {
       const auto a_l = curr_a->Get(i);
       const auto a_r = _a->Get(i);
-      if(a_l != a_r)
+      if(a_l != a_r) 
         return static_cast<int>(a_l > a_r) - static_cast<int>(a_l < a_r);
     }
     return 0;
