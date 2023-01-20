f657ba958caef73066faa24f1c3a42adb4ec8e98
le-michael <michael.le647@gmail.com>
2023-01-20 15:31:30 -0800

    Flatbuffers Version 23.1.20

--- tests/optional_scalars/ScalarStuff.kt
+++ tests/optional_scalars/ScalarStuff.kt
@@ -210,7 +210,7 @@ class ScalarStuff : Table() {
             return if(o != 0) bb.get(o + bb_pos) else 1
         }
     companion object {
-        fun validateVersion() = Constants.FLATBUFFERS_23_1_4()
+        fun validateVersion() = Constants.FLATBUFFERS_23_1_20()
         fun getRootAsScalarStuff(_bb: ByteBuffer): ScalarStuff = getRootAsScalarStuff(_bb, ScalarStuff())
         fun getRootAsScalarStuff(_bb: ByteBuffer, obj: ScalarStuff): ScalarStuff {
             _bb.order(ByteOrder.LITTLE_ENDIAN)
