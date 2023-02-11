dccd644a1ad9cc0e43f640654ff4204551ba1f8b
José Luis Millán <jmillan@aliax.net>
2023-02-11 18:24:02 +0100

    only export object based classes for structs
    Enums are not elegible.

--- src/idl_gen_ts.cpp
+++ src/idl_gen_ts.cpp
@@ -255,7 +255,6 @@ class TsGenerator : public BaseGenerator {
 
     for (const auto &it : ns_defs_) {
       code = "// " + std::string(FlatBuffersGeneratedWarning()) + "\n\n";
-
       // export all definitions in ns entry point module
       int export_counter = 0;
       for (const auto &def : it.second.definitions) {
@@ -281,8 +280,11 @@ class TsGenerator : public BaseGenerator {
         base_name_rel += base_file_name;
         auto ts_file_path_rel = base_name_rel + ".ts";
         auto type_name = def.first;
+        auto fully_qualified_type_name =
+            it.second.ns->GetFullyQualifiedName(type_name);
+        auto is_struct = parser_.structs_.Lookup(fully_qualified_type_name);
         code += "export { " + type_name;
-        if (parser_.opts.generate_object_based_api) {
+        if (parser_.opts.generate_object_based_api && is_struct) {
           code += ", " + type_name + parser_.opts.object_suffix;
         }
         code += " } from '";
