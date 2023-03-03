c86f82da63d2e5456aa2d6993cdb2cd87a8b5b20
José Luis Millán <jmillan@aliax.net>
2023-03-03 08:35:59 +0100

    TS/JS: Export object based classes on entry (#7822)
    * TS/JS: Export object based classes on entry
    
    Along with the non object ones, for consistency. This is a regression
    introduced recently.
    
    Before:
     `export { UpdateSettingsRequest } from './worker/update-settings-request.js';`
    
    Now:
     `export { UpdateSettingsRequest, UpdateSettingsRequestT } from './worker/update-settings-request.js';`
    
    * only export object based classes for structs
    
    Enums are not elegible.
    
    ---------
    
    Co-authored-by: Björn Harrtell <bjornharrtell@users.noreply.github.com>
    Co-authored-by: Derek Bailey <derekbailey@google.com>

--- tests/ts/arrays_test_complex/my-game/example.d.ts
+++ tests/ts/arrays_test_complex/my-game/example.d.ts
@@ -1,6 +1,6 @@
-export { ArrayStruct } from './example/array-struct.js';
-export { ArrayTable } from './example/array-table.js';
-export { InnerStruct } from './example/inner-struct.js';
-export { NestedStruct } from './example/nested-struct.js';
-export { OuterStruct } from './example/outer-struct.js';
+export { ArrayStruct, ArrayStructT } from './example/array-struct.js';
+export { ArrayTable, ArrayTableT } from './example/array-table.js';
+export { InnerStruct, InnerStructT } from './example/inner-struct.js';
+export { NestedStruct, NestedStructT } from './example/nested-struct.js';
+export { OuterStruct, OuterStructT } from './example/outer-struct.js';
 export { TestEnum } from './example/test-enum.js';
--- tests/ts/monster_test.d.ts
+++ tests/ts/monster_test.d.ts
@@ -1,2 +1,2 @@
-export { TableA } from './table-a.js';
+export { TableA, TableAT } from './table-a.js';
 export * as MyGame from './my-game.js';
