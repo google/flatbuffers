2f88628f7763e352f3ca1db89e9c6847ef81c3e0
Pavlo Bashmakov <157482+bexcite@users.noreply.github.com>
2023-03-25 12:45:56 -0700

    Rare fix: kick fix test

--- tests/alignment_test.cpp
+++ tests/alignment_test.cpp
@@ -3,25 +3,46 @@
 #include "tests/alignment_test_generated.h"
 #include "flatbuffers/flatbuffer_builder.h"
 #include "test_assert.h"
+#include <iostream>
 
 namespace flatbuffers {
 namespace tests {
 
 void AlignmentTest() {
+  std::cout << "uo! 1\n";
+  
   FlatBufferBuilder builder;
 
   BadAlignmentLarge large;
   Offset<OuterLarge> outer_large = CreateOuterLarge(builder, &large);
 
-  BadAlignmentSmall *small;
-  Offset<Vector<const BadAlignmentSmall *>> small_offset =
-      builder.CreateUninitializedVectorOfStructs(9, &small);
-  (void)small;  // We do not have to write data to trigger the test failure
+  // BadAlignmentSmall *small;
+  // Offset<Vector<const BadAlignmentSmall *>> small_offset =
+  //     builder.CreateUninitializedVectorOfStructs(9, &small);
+  // (void)small; // We do not have to write data to trigger the test failure
+
+  // Offset<Vector<const BadAlignmentSmall *>> small_offset =
+  //     builder.CreateUninitializedVectorOfStructs(9, &small);
+
+  // Offset<BadAlignmentRoot> root =
+  //     CreateBadAlignmentRoot(builder, outer_large, small_offset);
+
+  std::vector<BadAlignmentSmall> small_vector{{2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1}};
 
   Offset<BadAlignmentRoot> root =
-      CreateBadAlignmentRoot(builder, outer_large, small_offset);
+    CreateBadAlignmentRootDirect(builder, outer_large, &small_vector);
 
   builder.Finish(root);
+
+  auto root_msg =
+      flatbuffers::GetRoot<BadAlignmentRoot>(builder.GetBufferPointer());
+
+  std::cout << "msg.size = " << root_msg->small()->size() << std::endl;
+  for (size_t i = 0; i < root_msg->small()->size(); ++i) {
+    std::cout << "msg.small[" << i
+              << "] = " << (int)root_msg->small()->Get(i)->var_0() << ":"
+              << (int)root_msg->small()->Get(i)->var_1() << std::endl;
+  }
 
   Verifier verifier(builder.GetBufferPointer(), builder.GetSize());
   TEST_ASSERT(VerifyBadAlignmentRootBuffer(verifier));
