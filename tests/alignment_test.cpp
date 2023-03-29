507d3ad3267ec2b1f2ae6a0d5cafd5d1965a746f
Pavlo Bashmakov <157482+bexcite@users.noreply.github.com>
2023-03-28 21:37:53 -0700

    Rare fix: separate test

--- tests/alignment_test.cpp
+++ tests/alignment_test.cpp
@@ -14,42 +14,49 @@ void AlignmentTest() {
   BadAlignmentLarge large;
   Offset<OuterLarge> outer_large = CreateOuterLarge(builder, &large);
 
-  // BadAlignmentSmall *small;
-  // Offset<Vector<const BadAlignmentSmall *>> small_offset =
-  //     builder.CreateUninitializedVectorOfStructs(9, &small);
-  // (void)small; // We do not have to write data to trigger the test failure
-
-  // Offset<Vector<const BadAlignmentSmall *>> small_offset =
-  //     builder.CreateUninitializedVectorOfStructs(9, &small);
-
-  // Offset<BadAlignmentRoot> root =
-  //     CreateBadAlignmentRoot(builder, outer_large, small_offset);
-
-  std::vector<BadAlignmentSmall> small_vector{{2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1}};
+  BadAlignmentSmall *small;
+  Offset<Vector<const BadAlignmentSmall *>> small_offset =
+      builder.CreateUninitializedVectorOfStructs(9, &small);
+  (void)small; // We do not have to write data to trigger the test failure
 
   Offset<BadAlignmentRoot> root =
-    CreateBadAlignmentRootDirect(builder, outer_large, &small_vector);
-
-  // builder.ForceVectorAlignment(small_vector.size(), sizeof(BadAlignmentSmall), 8);
-  // auto small__ = builder.CreateVectorOfStructs<BadAlignmentSmall>(small_vector);
-  // auto small__ = CreateVectorOfStructs<BadAlignmentSmall>(builder, small_vector);
-  // Offset<BadAlignmentRoot> root =
-  //     CreateBadAlignmentRoot(builder, outer_large, small__);
+      CreateBadAlignmentRoot(builder, outer_large, small_offset);
 
   builder.Finish(root);
 
-  auto root_msg =
-      flatbuffers::GetRoot<BadAlignmentRoot>(builder.GetBufferPointer());
-
-  std::cout << "msg.size = " << root_msg->small()->size() << std::endl;
-  for (size_t i = 0; i < root_msg->small()->size(); ++i) {
-    std::cout << "msg.small[" << i
-              << "] = " << (int)root_msg->small()->Get(i)->var_0() << ":"
-              << (int)root_msg->small()->Get(i)->var_1() << std::endl;
-  }
-
   Verifier verifier(builder.GetBufferPointer(), builder.GetSize());
   TEST_ASSERT(VerifyBadAlignmentRootBuffer(verifier));
+
+  builder.Clear();
+
+  std::vector<JustSmallStruct> small_vector = {
+    { 2, 1 }, { 3, 1 }, { 4, 1 }, { 5, 1 }, { 6, 1 }
+  };
+  Offset<SmallStructs> small_structs_root =
+      CreateSmallStructsDirect(builder, &small_vector);
+
+  builder.Finish(small_structs_root);
+
+  Verifier verifier_small_structs(builder.GetBufferPointer(),
+                                  builder.GetSize());
+  TEST_ASSERT(verifier_small_structs.VerifyBuffer<SmallStructs>(nullptr));
+
+  auto root_msg =
+      flatbuffers::GetRoot<SmallStructs>(builder.GetBufferPointer());
+
+  std::cout << "msg.size = " << root_msg->small_structs()->size() << std::endl;
+  TEST_EQ(root_msg->small_structs()->size(), small_vector.size());
+
+  for (size_t i = 0; i < root_msg->small_structs()->size(); ++i) {
+    std::cout << "msg.small_structs[" << i
+              << "] = " << (int)root_msg->small_structs()->Get(i)->var_0()
+              << ":" << (int)root_msg->small_structs()->Get(i)->var_1()
+              << std::endl;
+    TEST_EQ(root_msg->small_structs()->Get(i)->var_0(),
+            small_vector[i].var_0());
+    TEST_EQ(root_msg->small_structs()->Get(i)->var_1(),
+            small_vector[i].var_1());
+  }
 }
 
 }  // namespace tests
