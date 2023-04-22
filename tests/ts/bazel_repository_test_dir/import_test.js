8e5dacefc985c6b3d26112463afc9a2ff5a2a52a
Philipp Schrader <philipp.schrader@gmail.com>
2023-04-21 21:35:18 -0700

    Get test working on its own

--- tests/ts/bazel_repository_test_dir/import_test.js
+++ tests/ts/bazel_repository_test_dir/import_test.js
@@ -1,7 +1,7 @@
 import assert from 'assert'
 import * as flatbuffers from 'flatbuffers'
 
-import two_cjs from './two.cjs'
+import two_cjs from './two_generated.cjs'
 
 const bazel_repository_test = two_cjs.bazel_repository_test;
 
@@ -9,10 +9,18 @@ function main() {
   // Validate building a table.
   var fbb = new flatbuffers.Builder(1);
 
+  bazel_repository_test.One.startOne(fbb);
+  bazel_repository_test.One.addInformation(fbb, 42);
+  var one = bazel_repository_test.One.endOne(fbb);
+
   bazel_repository_test.Two.startTwo(fbb);
+  bazel_repository_test.Two.addOne(fbb, one);
   var two = bazel_repository_test.Two.endTwo(fbb);
 
   fbb.finish(two);
+
+  // Call as a sanity check. Would be better to validate actual output here.
+  fbb.asUint8Array();
 
   console.log('FlatBuffers bazel repository test: completed successfully');
 }
