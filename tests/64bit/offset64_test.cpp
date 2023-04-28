08302332d3e904182dcbcf8372182b7e47733701
Derek Bailey <derekbailey@google.com>
2023-04-28 15:29:04 -0700

    ensure test is for a big buffer

--- tests/64bit/offset64_test.cpp
+++ tests/64bit/offset64_test.cpp
@@ -22,11 +22,10 @@ namespace flatbuffers {
 namespace tests {
 
 void Offset64Test() {
-  flatbuffers::FlatBufferBuilder64 fbb;
+  flatbuffers::FlatBufferBuilder64 builder;
 
-  size_t far_vector_size = 1LL << 2;
-  size_t big_vector_size =
-      2LL << 2;  // boost to a large number to make a big buffer
+  const size_t far_vector_size = 1LL << 2;
+  const size_t big_vector_size = 1LL << 31;
 
   {
     // First create the vectors that will be copied to the buffer.
@@ -43,44 +42,42 @@ void Offset64Test() {
     // Then serialize all the fields that have 64-bit offsets, as these must be
     // serialized before any 32-bit fields are added to the buffer.
     const Offset64<Vector<uint8_t>> far_vector_offset =
-        fbb.CreateVector64<Vector>(far_data);
+        builder.CreateVector64<Vector>(far_data);
 
     const Offset64<String> far_string_offset =
-        fbb.CreateString<Offset64>("some far string");
+        builder.CreateString<Offset64>("some far string");
 
     const Offset64<Vector64<uint8_t>> big_vector_offset =
-        fbb.CreateVector64(big_data);
+        builder.CreateVector64(big_data);
 
     // Now that we are done with the 64-bit fields, we can create and add the
     // normal fields.
     const Offset<String> near_string_offset =
-        fbb.CreateString("some near string");
+        builder.CreateString("some near string");
 
     // Finish by building the root table by passing in all the offsets.
     const Offset<RootTable> root_table_offset =
-        CreateRootTable(fbb, far_vector_offset, 0, far_string_offset,
+        CreateRootTable(builder, far_vector_offset, 0, far_string_offset,
                         big_vector_offset, near_string_offset);
 
     // Finish the buffer.
-    fbb.Finish(root_table_offset);
+    builder.Finish(root_table_offset);
+
+    // Ensure the buffer is big.
+    TEST_ASSERT(builder.GetSize() > FLATBUFFERS_MAX_BUFFER_SIZE);
 
     Verifier::Options options;
     // Allow the verifier to verify 64-bit buffers.
     options.max_size = FLATBUFFERS_MAX_64_BUFFER_SIZE;
     options.assert = true;
 
-    // TODO(derekbailey): remove once we don't need to analyze the binary.
-    // SaveFile("tests/64bit/offset64_test_cpp.bin",
-    //          reinterpret_cast<const char *>(fbb.GetBufferPointer()),
-    //          fbb.GetSize(), true);
-
-    Verifier verifier(fbb.GetBufferPointer(), fbb.GetSize(), options);
+    Verifier verifier(builder.GetBufferPointer(), builder.GetSize(), options);
 
     TEST_EQ(VerifyRootTableBuffer(verifier), true);
   }
 
   {
-    const RootTable *root_table = GetRootTable(fbb.GetBufferPointer());
+    const RootTable *root_table = GetRootTable(builder.GetBufferPointer());
 
     // Expect the far vector to be properly sized.
     TEST_EQ(root_table->far_vector()->size(), far_vector_size);
--- tests/64bit/run.sh
+++ tests/64bit/run.sh
@@ -5,5 +5,3 @@ make -j
 ./flatc -b -o tests/64bit tests/64bit/test_64bit.fbs tests/64bit/test_64bit.json
 
 ./flatc -o tests/64bit --annotate-sparse-vectors --annotate tests/64bit/test_64bit.fbs -- tests/64bit/test_64bit.bin
-
-./flatc -o tests/64bit --annotate-sparse-vectors --annotate tests/64bit/test_64bit.fbs -- tests/64bit/offset64_test_cpp.bin
