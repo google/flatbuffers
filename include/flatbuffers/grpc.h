8019ffc27d349e06f863a3a849b642aad3f64a32
Derek Bailey <derekbailey@google.com>
2023-05-10 16:41:39 -0700

    remove unneeed grpc include

--- include/flatbuffers/grpc.h
+++ include/flatbuffers/grpc.h
@@ -20,7 +20,6 @@
 // Helper functionality to glue FlatBuffers and GRPC.
 
 #include "flatbuffers/flatbuffers.h"
-#include "grpc/byte_buffer_reader.h"
 #include "grpcpp/support/byte_buffer.h"
 #include "grpcpp/support/slice.h"
 
