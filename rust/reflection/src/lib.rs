73696c4295602aaf31cce24a67992e33dcef1b52
Chan Wang <chanwang@google.com>
2023-09-25 08:12:18 +0000

    #Rust Get root table of a buffer and access field with schema

--- rust/reflection/src/lib.rs
+++ rust/reflection/src/lib.rs
@@ -16,3 +16,121 @@
 
 mod reflection_generated;
 pub use crate::reflection_generated::reflection;
+
+use flatbuffers::{Follow, ForwardsUOffset, Table};
+use reflection_generated::reflection::{BaseType, Field};
+
+use core::mem::size_of;
+use num::traits::float::Float;
+use num::traits::int::PrimInt;
+use num::traits::FromPrimitive;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum FlatbufferError {
+    #[error("Failed to get data of type {0} from field of type {1}")]
+    FieldTypeMismatch(String, String),
+}
+
+pub type FlatbufferResult<T, E = FlatbufferError> = core::result::Result<T, E>;
+
+/// Gets the root table from a trusted Flatbuffer.
+///
+/// # Safety
+///
+/// Flatbuffers accessors do not perform validation checks before accessing. Users
+/// must trust [data] contains a valid flatbuffer. Reading unchecked buffers may cause panics or even UB.
+pub unsafe fn get_any_root(data: &[u8]) -> Table {
+    <ForwardsUOffset<Table>>::follow(data, 0)
+}
+
+/// Gets an integer table field given its exact type. Returns default integer value if the field is not set. Returns [None] if no default value is found. Returns error if the type size doesn't match.
+///
+/// # Safety
+///
+/// The value of the corresponding slot must have type T
+pub unsafe fn get_field_integer<'a, T: Follow<'a, Inner = T> + PrimInt + FromPrimitive + 'a>(
+    table: &'a Table,
+    field: &Field,
+) -> FlatbufferResult<Option<T>> {
+    if size_of::<T>() != get_type_size(field.type_().base_type()) {
+        return Err(FlatbufferError::FieldTypeMismatch(
+            std::any::type_name::<T>().to_string(),
+            field
+                .type_()
+                .base_type()
+                .variant_name()
+                .unwrap_or_default()
+                .to_string(),
+        ));
+    }
+
+    let default = T::from_i64(field.default_integer());
+    Ok(table.get::<T>(field.offset(), default))
+}
+
+/// Gets a floating point table field given its exact type. Returns default float value if the field is not set. Returns [None] if no default value is found. Returns error if the type doesn't match.
+///
+/// # Safety
+///
+/// The value of the corresponding slot must have type T
+pub unsafe fn get_field_float<'a, T: Follow<'a, Inner = T> + Float + 'a>(
+    table: &'a Table,
+    field: &Field,
+) -> FlatbufferResult<Option<T>> {
+    if size_of::<T>() != get_type_size(field.type_().base_type()) {
+        return Err(FlatbufferError::FieldTypeMismatch(
+            std::any::type_name::<T>().to_string(),
+            field
+                .type_()
+                .base_type()
+                .variant_name()
+                .unwrap_or_default()
+                .to_string(),
+        ));
+    }
+
+    let default = T::from(field.default_real());
+    Ok(table.get::<T>(field.offset(), default))
+}
+
+/// Gets a String table field given its exact type. Returns empty string if the field is not set. Returns [None] if no default value is found. Returns error if the type size doesn't match.
+///
+/// # Safety
+///
+/// The value of the corresponding slot must have type String
+pub unsafe fn get_field_string<'a>(
+    table: &'a Table,
+    field: &Field,
+) -> FlatbufferResult<Option<&'a str>> {
+    if field.type_().base_type() != BaseType::String {
+        return Err(FlatbufferError::FieldTypeMismatch(
+            String::from("String"),
+            field
+                .type_()
+                .base_type()
+                .variant_name()
+                .unwrap_or_default()
+                .to_string(),
+        ));
+    }
+
+    Ok(table.get::<ForwardsUOffset<&'a str>>(field.offset(), Some("")))
+}
+
+/// Returns the size of a scalar type in the `BaseType` enum. In the case of structs, returns the size of their offset (`UOffsetT`) in the buffer.
+fn get_type_size(base_type: BaseType) -> usize {
+    match base_type {
+        BaseType::UType | BaseType::Bool | BaseType::Byte | BaseType::UByte => 1,
+        BaseType::Short | BaseType::UShort => 2,
+        BaseType::Int
+        | BaseType::UInt
+        | BaseType::Float
+        | BaseType::String
+        | BaseType::Vector
+        | BaseType::Obj
+        | BaseType::Union => 4,
+        BaseType::Long | BaseType::ULong | BaseType::Double | BaseType::Vector64 => 8,
+        _ => 0,
+    }
+}
--- tests/rust_reflection_test/src/lib.rs
+++ tests/rust_reflection_test/src/lib.rs
@@ -1,7 +1,15 @@
-use flatbuffers_reflection::reflection::{root_as_schema, BaseType};
+use flatbuffers_reflection::reflection::{root_as_schema, BaseType, Field};
+use flatbuffers_reflection::{get_any_root, get_field_float, get_field_integer, get_field_string};
+
+use flatbuffers::FlatBufferBuilder;
 
 use std::fs::File;
 use std::io::Read;
+
+#[allow(dead_code, unused_imports)]
+#[path = "../../monster_test/mod.rs"]
+mod monster_test_generated;
+pub use monster_test_generated::my_game;
 
 #[test]
 fn test_schema_correct_root_table() {
@@ -169,6 +177,165 @@ fn test_schema_correct_child_table_index() {
     assert_eq!(pos_table.name(), "MyGame.Example.Vec3");
 }
 
+#[test]
+fn test_buffer_integer_same_type_succeeds() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let i16_field = get_schema_field(&schema, "hp");
+
+    let value = unsafe { get_field_integer::<i16>(&root_table, &i16_field) };
+
+    assert!(value.is_ok());
+    assert_eq!(value.unwrap(), Some(32767));
+}
+
+#[test]
+fn test_buffer_integer_diff_type_same_size_succeeds() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let i16_field = get_schema_field(&schema, "hp");
+
+    let value = unsafe { get_field_integer::<u16>(&root_table, &i16_field) };
+
+    assert!(value.is_ok());
+    assert_eq!(value.unwrap(), Some(32767));
+}
+
+#[test]
+fn test_buffer_integer_diff_size_fails() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let i16_field = get_schema_field(&schema, "hp");
+
+    let value = unsafe { get_field_integer::<i64>(&root_table, &i16_field) };
+
+    assert!(value.is_err());
+    assert_eq!(
+        value.unwrap_err().to_string(),
+        "Failed to get data of type i64 from field of type Short"
+    );
+}
+
+#[test]
+fn test_buffer_float_same_type_succeeds() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let f32_field = get_schema_field(&schema, "testf");
+
+    let value = unsafe { get_field_float::<f32>(&root_table, &f32_field) };
+
+    assert!(value.is_ok());
+    assert_eq!(value.unwrap(), Some(3.14));
+}
+
+#[test]
+fn test_buffer_float_diff_type_fails() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let f32_field = get_schema_field(&schema, "testf");
+
+    let value = unsafe { get_field_float::<f64>(&root_table, &f32_field) };
+
+    assert!(value.is_err());
+    assert_eq!(
+        value.unwrap_err().to_string(),
+        "Failed to get data of type f64 from field of type Float"
+    );
+}
+
+#[test]
+fn test_buffer_string_same_type_succeeds() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let string_field = get_schema_field(&schema, "name");
+
+    let value = unsafe { get_field_string(&root_table, &string_field) };
+
+    assert!(value.is_ok());
+    assert_eq!(value.unwrap(), Some("MyMonster"));
+}
+
+#[test]
+fn test_buffer_string_diff_type_fails() {
+    let buffer = create_test_buffer();
+    let root_table = unsafe { get_any_root(&buffer) };
+    let schema = load_file_as_buffer("../monster_test.bfbs");
+    let f32_field = get_schema_field(&schema, "testf");
+
+    let value = unsafe { get_field_string(&root_table, &f32_field) };
+
+    assert!(value.is_err());
+    assert_eq!(
+        value.unwrap_err().to_string(),
+        "Failed to get data of type String from field of type Float"
+    );
+}
+
 fn load_file_as_buffer(path: &str) -> Vec<u8> {
     std::fs::read(path).unwrap()
+}
+
+fn get_schema_field<'a>(schema_buffer: &'a Vec<u8>, field_name: &'a str) -> Field<'a> {
+    let schema = root_as_schema(schema_buffer.as_slice()).unwrap();
+    let root_table = schema.root_table().unwrap();
+    let fields = root_table.fields();
+    fields
+        .lookup_by_key(field_name, |field, key| field.key_compare_with_value(key))
+        .unwrap()
+}
+
+fn create_test_buffer() -> Vec<u8> {
+    let mut builder = FlatBufferBuilder::new();
+    let mon = {
+        let s0 = builder.create_string("test1");
+        let s1 = builder.create_string("test2");
+        let fred_name = builder.create_string("Fred");
+
+        // can't inline creation of this Vec3 because we refer to it by reference, so it must live
+        // long enough to be used by MonsterArgs.
+        let pos = my_game::example::Vec3::new(
+            1.0,
+            2.0,
+            3.0,
+            3.0,
+            my_game::example::Color::Green,
+            &my_game::example::Test::new(5i16, 6i8),
+        );
+
+        let args = my_game::example::MonsterArgs {
+            hp: 32767,
+            testf: 3.14,
+            mana: 150,
+            name: Some(builder.create_string("MyMonster")),
+            pos: Some(&pos),
+            test_type: my_game::example::Any::Monster,
+            test: Some(
+                my_game::example::Monster::create(
+                    &mut builder,
+                    &my_game::example::MonsterArgs {
+                        name: Some(fred_name),
+                        ..Default::default()
+                    },
+                )
+                .as_union_value(),
+            ),
+            inventory: Some(builder.create_vector(&[0u8, 1, 2, 3, 4])),
+            test4: Some(builder.create_vector(&[
+                my_game::example::Test::new(10, 20),
+                my_game::example::Test::new(30, 40),
+            ])),
+            testarrayofstring: Some(builder.create_vector(&[s0, s1])),
+            ..Default::default()
+        };
+        my_game::example::Monster::create(&mut builder, &args)
+    };
+    my_game::example::finish_monster_buffer(&mut builder, mon);
+    let (flatbuf, start_loc) = builder.mut_finished_buffer();
+    flatbuf[start_loc..].to_vec()
 }
