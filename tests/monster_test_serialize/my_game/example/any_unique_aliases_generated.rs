205285c35c164c0368c913b3f611cf8d672bc103
adsnaider <adsnaider@gmail.com>
2023-10-07 14:28:52 -0500

    [Rust] Add the Allocator trait for the builder API (#8106)
    * Add an Allocator trait for FlatBufferBuilder
    
    * Update rust generated code

--- tests/monster_test_serialize/my_game/example/any_unique_aliases_generated.rs
+++ tests/monster_test_serialize/my_game/example/any_unique_aliases_generated.rs
@@ -138,7 +138,7 @@ impl AnyUniqueAliasesT {
       Self::M2(_) => AnyUniqueAliases::M2,
     }
   }
-  pub fn pack(&self, fbb: &mut flatbuffers::FlatBufferBuilder) -> Option<flatbuffers::WIPOffset<flatbuffers::UnionWIPOffset>> {
+  pub fn pack<'b, A: flatbuffers::Allocator + 'b>(&self, fbb: &mut flatbuffers::FlatBufferBuilder<'b, A>) -> Option<flatbuffers::WIPOffset<flatbuffers::UnionWIPOffset>> {
     match self {
       Self::NONE => None,
       Self::M(v) => Some(v.pack(fbb).as_union_value()),
