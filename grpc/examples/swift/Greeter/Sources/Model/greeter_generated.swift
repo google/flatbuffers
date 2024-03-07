d1867fb4cf7d64b2d3d23b67e86df6ae5d9f993b
Derek Bailey <derekbailey@google.com>
2024-03-07 06:52:51 +0000

    FlatBuffers Version v24.3.6

--- grpc/examples/swift/Greeter/Sources/Model/greeter_generated.swift
+++ grpc/examples/swift/Greeter/Sources/Model/greeter_generated.swift
@@ -6,7 +6,7 @@ import FlatBuffers
 
 public struct models_HelloReply: FlatBufferObject, Verifiable {
 
-  static func validateVersion() { FlatBuffersVersion_23_5_26() }
+  static func validateVersion() { FlatBuffersVersion_24_3_6() }
   public var __buffer: ByteBuffer! { return _accessor.bb }
   private var _accessor: Table
 
@@ -53,7 +53,7 @@ extension models_HelloReply: Encodable {
 
 public struct models_HelloRequest: FlatBufferObject, Verifiable {
 
-  static func validateVersion() { FlatBuffersVersion_23_5_26() }
+  static func validateVersion() { FlatBuffersVersion_24_3_6() }
   public var __buffer: ByteBuffer! { return _accessor.bb }
   private var _accessor: Table
 
