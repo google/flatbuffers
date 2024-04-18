1708cb0be2cd731fb48031f17e2ee89748337ccf
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2024-04-18 22:17:07 +0200

    Fixes Pool key search algorithm, causing key and string sharing to fail.
    Binary search requires the pool to be sorted, which wasn't the case. Insertion now happens sorted, at the index returned by _LowerBound.

--- python/flatbuffers/flexbuffers.py
+++ python/flatbuffers/flexbuffers.py
@@ -960,10 +960,12 @@ class Pool:
 
   def FindOrInsert(self, data, offset):
     do = data, offset
-    index = _BinarySearch(self._pool, do, lambda a, b: a[0] < b[0])
-    if index != -1:
+    pred = lambda a, b: a[0] < b[0]
+    index = _LowerBound(self._pool, do, pred)
+    if index != len(self._pool) and not pred(do, self._pool[index]):
       _, offset = self._pool[index]
       return offset
+    # Insert sorted using index found by _LowerBound
     self._pool.insert(index, do)
     return None
 
