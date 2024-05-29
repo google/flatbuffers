bf83bfc7e321e703e57b2b3b8a7012b209cba006
Anton Bobukh <abobukh@google.com>
2024-05-29 19:59:03 +0000

    [BinaryAnnotator] Update the include paths.

--- src/binary_annotator.h
+++ src/binary_annotator.h
@@ -28,10 +28,10 @@
 #include <utility>
 #include <vector>
 
-#include "include/flatbuffers/base.h"
-#include "include/flatbuffers/reflection.h"
-#include "include/flatbuffers/reflection_generated.h"
-#include "include/flatbuffers/stl_emulation.h"
+#include "flatbuffers/base.h"
+#include "flatbuffers/reflection.h"
+#include "flatbuffers/reflection_generated.h"
+#include "flatbuffers/stl_emulation.h"
 
 namespace flatbuffers {
 
