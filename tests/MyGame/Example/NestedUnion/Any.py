5af2115527ddfeef58d25501e75dadc4d68869bd
Felix <hi@l33t.name>
2024-06-03 17:39:14 +0200

    [Python] Render enums as Python IntEnum (#8145)
    This allows enums to be type check with mypy.
    They will still behave like ints ->
    > IntEnum is the same as Enum,
    > but its members are also integers and can be used anywhere
    > that an integer can be used.
    > If any integer operation is performed with an IntEnum member,
    > the resulting value loses its enumeration status.
    https://docs.python.org/3/library/enum.html#enum.IntEnum
    
    Only if the --python-typing flag is set.

--- tests/MyGame/Example/NestedUnion/Any.py
+++ tests/MyGame/Example/NestedUnion/Any.py
@@ -11,10 +11,10 @@ def AnyCreator(unionType, table):
     from flatbuffers.table import Table
     if not isinstance(table, Table):
         return None
-    if unionType == Any().Vec3:
+    if unionType == Any.Vec3:
         import MyGame.Example.NestedUnion.Vec3
         return MyGame.Example.NestedUnion.Vec3.Vec3T.InitFromBuf(table.Bytes, table.Pos)
-    if unionType == Any().TestSimpleTableWithEnum:
+    if unionType == Any.TestSimpleTableWithEnum:
         import MyGame.Example.NestedUnion.TestSimpleTableWithEnum
         return MyGame.Example.NestedUnion.TestSimpleTableWithEnum.TestSimpleTableWithEnumT.InitFromBuf(table.Bytes, table.Pos)
     return None
