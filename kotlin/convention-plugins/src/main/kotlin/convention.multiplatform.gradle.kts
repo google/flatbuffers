5fba53905ea5045c1adb4ff51d21e19cb5be589a
Didier Villevalois <ptitjes@free.fr>
2025-02-14 13:54:47 +0100

    [KMP] modernize Gradle build further

--- kotlin/convention-plugins/src/main/kotlin/convention.multiplatform.gradle.kts
+++ kotlin/convention-plugins/src/main/kotlin/convention.multiplatform.gradle.kts
@@ -1,3 +1,28 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import java.nio.charset.*
+
 plugins {
   kotlin("multiplatform")
+}
+
+tasks.withType<KotlinCompile<*>> {
+  kotlinOptions {
+    // https://kotlinlang.org/docs/whatsnew13.html#progressive-mode
+    freeCompilerArgs += "-progressive"
+  }
+}
+
+tasks.withType<KotlinJvmCompile> {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_1_8)
+    freeCompilerArgs.add("-Xjvm-default=all")
+  }
+}
+
+tasks.withType<JavaCompile> {
+  options.encoding = StandardCharsets.UTF_8.toString()
+  sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+  targetCompatibility = JavaVersion.VERSION_1_8.toString()
 }
--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -1,4 +1,20 @@
 rootProject.name = "flatbuffers-kotlin"
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+}
+
+dependencyResolutionManagement {
+  @Suppress("UnstableApiUsage")
+  repositories {
+    mavenCentral()
+  }
+}
+
 includeBuild("convention-plugins")
 include("flatbuffers-kotlin")
 include("benchmark")
