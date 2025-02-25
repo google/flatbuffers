e3c966c9b927afd04077135632fa1e25d587c8f9
fawdlstty <f@fawdlstty.com>
2025-02-25 23:01:51 +0800

    update generated files

--- tests/monster_test_serialize/my_game/example/type_aliases_generated.rs
+++ tests/monster_test_serialize/my_game/example/type_aliases_generated.rs
@@ -22,7 +22,7 @@ impl<'a> flatbuffers::Follow<'a> for TypeAliases<'a> {
   type Inner = TypeAliases<'a>;
   #[inline]
   unsafe fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
-    Self { _tab: flatbuffers::Table::new(buf, loc) }
+    Self { _tab: unsafe { flatbuffers::Table::new(buf, loc) } }
   }
 }
 
