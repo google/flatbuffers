5b30be9b58c9dfd20ccad7d0b0c675f7fd3f5962
fliiiix <hi@l33t.name>
2025-06-24 08:20:05 +0200

    [Python] Avoid double flatbuffers include in pyi files

--- tests/MyGame/Example/NestedUnion/Color.pyi
+++ tests/MyGame/Example/NestedUnion/Color.pyi
@@ -3,7 +3,6 @@ from __future__ import annotations
 import flatbuffers
 import numpy as np
 
-import flatbuffers
 import typing
 
 uoffset: typing.TypeAlias = flatbuffers.number_types.UOffsetTFlags.py_type
