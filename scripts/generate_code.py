2a3768933ecb2d91b23629b2134c79ee48844274
Connor Smith <cosmith@nvidia.com>
2025-08-19 12:15:17 -0700

    Fix --gen-compare to not generate comparators for native types.
    Per the definition of --gen-compare: only generate comparators for object API
    generated structs. Types annoated with native_type must define their own
    comparators if `--gen-compare` is enabled.
    
    Also enables --gen-compare for native_type_test and fixes the test by adding a
    comparator for the Native::Vector3D type.

--- scripts/generate_code.py
+++ scripts/generate_code.py
@@ -334,7 +334,7 @@ flatc(
 )
 
 flatc(
-    ["--cpp", "--gen-mutable", "--gen-object-api", "--reflect-names"],
+    ["--cpp", "--gen-compare", "--gen-mutable", "--gen-object-api", "--reflect-names"],
     schema="native_type_test.fbs",
 )
 
