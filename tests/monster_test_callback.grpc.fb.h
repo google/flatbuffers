deb3d93454e1782a133186876000d3272634789c
Shashank <117587128+shankeleven@users.noreply.github.com>
2025-08-29 04:19:27 +0530

    gRPC callbackService support added (#8666)
    * grpc callbackService support added
    
    Signed-off-by: shankeleven <shashanksati11@gmail.com>
    
    * tests: regenerate C++ gRPC golden with --grpc-callback-api (CallbackService & async_ reactor APIs); update formatting and method placement
    
    ---------
    
    Signed-off-by: shankeleven <shashanksati11@gmail.com>
    Co-authored-by: Wouter van Oortmerssen <aardappel@gmail.com>

---
+++ tests/monster_test_callback.grpc.fb.h
@@ -0,0 +1,4 @@
+// Callback variant copy of monster_test.grpc.fb.h generated with
+// --grpc-callback-api (kept separate as a golden for callback API feature
+// evolution)
+#include "monster_test.grpc.fb.h"
