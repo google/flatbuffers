e6a4bb1fd4874a6932760189b9a7cd900fb461f7
Justin Davis <jtdavis777@gmail.com>
2025-11-05 09:14:36 -0500

    simplify wireshark_numbers a bit

--- wireshark/00_wireshark_numbers.lua
+++ wireshark/00_wireshark_numbers.lua
@@ -1,22 +1,4 @@
-function GetNumberBuffer(buffer, offset, proto_field)
-    local field_size = {
-        [fb_bool] = 1,
-        [fb_uint8] = 1,
-        [fb_int8] = 1,
-        [fb_uint16] = 2,
-        [fb_int16] = 2,
-        [fb_uint32] = 4,
-        [fb_int32] = 4,
-        [fb_float32] = 4,
-        [fb_uint64] = 8,
-        [fb_int64] = 8,
-        [fb_float64] = 8
-    }
-
-    return buffer(offset, field_size[proto_field])
-end
-
-local function AddFieldToTree(buffer, offset, tree, field_type, scalar_type, proto_field, default_value)
+local function AddFieldToTree(buffer, offset, tree, field_type, scalar_type, field_width, default_value)
     if offset == 0 then
         local subtree = tree:add(field_type, default_value)
         subtree:append_text(" [DEFAULT VALUE]")
@@ -26,7 +8,7 @@ local function AddFieldToTree(buffer, offset, tree, field_type, scalar_type, pro
         }
     end
 
-    local field_buffer = GetNumberBuffer(buffer, offset, proto_field)
+    local field_buffer = buffer(offset, field_width)
     local field_value = field_buffer["le_" .. scalar_type](field_buffer)
     return {
         tree = tree:add(field_type, field_buffer, field_value),
@@ -36,52 +18,52 @@ end
 
 function Parse_Uint8(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "uint",
-        fb_uint8, default_value)
+        1, default_value)
 end
 
 function Parse_Uint16(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "uint",
-        fb_uint16, default_value)
+        2, default_value)
 end
 
 function Parse_Uint32(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "uint",
-        fb_uint32, default_value)
+        4, default_value)
 end
 
 function Parse_Uint64(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "uint64",
-        fb_uint64, default_value)
+        8, default_value)
 end
 
 function Parse_Int8(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type,
-        "int", fb_int8, default_value)
+        "int", 1, default_value)
 end
 
 function Parse_Int16(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "int",
-        fb_int16, default_value)
+        2, default_value)
 end
 
 function Parse_Int32(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "int",
-        fb_int32, default_value)
+        4, default_value)
 end
 
 function Parse_Int64(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "int64",
-        fb_int64, default_value)
+        8, default_value)
 end
 
 function Parse_Float32(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "float",
-        fb_float32, default_value)
+        4, default_value)
 end
 
 function Parse_Float64(buffer, offset, tree, field_type, default_value)
     return AddFieldToTree(buffer, offset, tree, field_type, "double",
-        fb_float64, default_value)
+        8, default_value)
 end
 
 function Parse_Bool(buffer, offset, tree, field_type, default_value)
@@ -94,7 +76,7 @@ function Parse_Bool(buffer, offset, tree, field_type, default_value)
         }
     end
 
-    local bool_buffer = GetNumberBuffer(buffer, offset, fb_bool)
+    local bool_buffer = buffer(offset, 1)
     local bool_value = bool_buffer:le_uint()
     return {
         tree = tree:add(field_type, bool_buffer, bool_value),
