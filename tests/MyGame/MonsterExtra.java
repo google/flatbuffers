a5770508176874af47eb4e6cc981968f52e3b75d
Wakahisa <nevilledips@gmail.com>
2025-12-01 18:28:50 +0200

    [Java] Use Table's fully qualified path (#8729)
    * [Java] Use Table's fully qualified path
    
    When a table's name is called `Table`, the Java bindings generated result in an error due to there being  2 Tables. This fixes the issue by fully qualifyng the flatbuffers Table import.
    
    * Update codegen
    
    * Update generated Java code
    
    ---------
    
    Co-authored-by: Neville Dipale <neville@urbanlogiq.com>

--- tests/MyGame/MonsterExtra.java
+++ tests/MyGame/MonsterExtra.java
@@ -14,13 +14,12 @@ import com.google.flatbuffers.LongVector;
 import com.google.flatbuffers.ShortVector;
 import com.google.flatbuffers.StringVector;
 import com.google.flatbuffers.Struct;
-import com.google.flatbuffers.Table;
 import com.google.flatbuffers.UnionVector;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 @SuppressWarnings("unused")
-public final class MonsterExtra extends Table {
+public final class MonsterExtra extends com.google.flatbuffers.Table {
   public static void ValidateVersion() { Constants.FLATBUFFERS_25_9_23(); }
   public static MonsterExtra getRootAsMonsterExtra(ByteBuffer _bb) { return getRootAsMonsterExtra(_bb, new MonsterExtra()); }
   public static MonsterExtra getRootAsMonsterExtra(ByteBuffer _bb, MonsterExtra obj) { _bb.order(ByteOrder.LITTLE_ENDIAN); return (obj.__assign(_bb.getInt(_bb.position()) + _bb.position(), _bb)); }
