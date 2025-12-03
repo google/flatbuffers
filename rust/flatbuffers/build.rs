646a8bc96a01782fce535a81a2f633566f1aefdb
Cameron Mulhern <csmulhern@gmail.com>
2025-12-02 21:06:24 -0500

    Improves Rust code generation (#8564)
    * Fixes checks for serde features in flexbuffers crate
    
    * Removes unused MapReaderIndexer use statement
    
    * Fixes warning about nightly cfg usage
    
    Enabling a cfg attribute through cargo::rustc-cfg in build.rs should be coupled with a cargo::rust-check-cfg value so that the compiler knows about the custom cfg. See: https://doc.rust-lang.org/rustc/check-cfg/cargo-specifics.html#cargorustc-check-cfg-for-buildrsbuild-script.
    
    * Migrates usage of deprecated float constants
    
    This update fixes a compiler warning from use of the old constants.
    
    Constants like EPSILON are now directly on the float primitives (e.g. f32::EPSILON) rather than in the f32 module (std::f32::EPSILON).
    
    The new constants have existed since 1.43.0, which appears to be below the MSRV for the flatbuffers crate.
    
    * Fixes incorrect key in flatbuffers Cargo.toml
    
    The old code was using package.rust, which triggered a warning about an unused key:
    
    warning: flatbuffers/rust/flatbuffers/Cargo.toml: unused manifest key: package.rust
    
    The correct key for specifying MSRV is rust-version. See: https://doc.rust-lang.org/cargo/reference/rust-version.html#rust-version.
    
    ---------
    
    Co-authored-by: Wouter van Oortmerssen <aardappel@gmail.com>

--- rust/flatbuffers/build.rs
+++ rust/flatbuffers/build.rs
@@ -1,4 +1,4 @@
-use rustc_version::{version_meta, Channel};
+use rustc_version::{Channel, version_meta};
 
 fn main() {
     let version_meta = version_meta().unwrap();
@@ -6,6 +6,7 @@ fn main() {
     // To use nightly features we declare this and then we can use
     // #[cfg(nightly)]
     // for nightly only features
+    println!("cargo:rustc-check-cfg=cfg(nightly)");
     if version_meta.channel == Channel::Nightly {
         println!("cargo:rustc-cfg=nightly")
     }
