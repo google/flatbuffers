48c25fc117bacc2b6d972b85617e7d64b06e4791
Justin Davis <jtdavis777@gmail.com>
2025-12-06 18:02:20 -0500

    deprecate the two options which have no effect of their own

--- src/idl_gen_ts.cpp
+++ src/idl_gen_ts.cpp
@@ -118,7 +118,6 @@ class TsGenerator : public BaseGenerator {
     if (!parser_.opts.ts_omit_entrypoint) {
       generateEntry();
     }
-    if (!generateBundle()) return false;
     return true;
   }
 
@@ -323,29 +322,6 @@ class TsGenerator : public BaseGenerator {
         parser_.opts.file_saver->SaveFile(it.second.filepath.c_str(), code,
                                           false);
     }
-  }
-
-  bool generateBundle() {
-    if (parser_.opts.ts_flat_files) {
-      std::string inputpath;
-      std::string symbolic_name = file_name_;
-      inputpath = path_ + file_name_ + ".ts";
-      std::string bundlepath =
-          GeneratedFileName(path_, file_name_, parser_.opts);
-      bundlepath = bundlepath.substr(0, bundlepath.size() - 3) + ".js";
-      std::string cmd = "esbuild";
-      cmd += " ";
-      cmd += inputpath;
-      // cmd += " --minify";
-      cmd += " --format=cjs --bundle --outfile=";
-      cmd += bundlepath;
-      cmd += " --external:flatbuffers";
-      std::cout << "Entry point " << inputpath << " generated." << std::endl;
-      std::cout << "A single file bundle can be created using fx. esbuild with:"
-                << std::endl;
-      std::cout << "> " << cmd << std::endl;
-    }
-    return true;
   }
 
   // Generate a documentation comment, if available.
