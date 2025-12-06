6864d64717ada0168016107f035c6db3994e70ce
Justin Davis <jtdavis777@gmail.com>
2025-12-05 21:21:40 -0500

    create test that fails to compile

---
+++ tests/vector_table_naked_ptr_test.cpp
@@ -0,0 +1,12 @@
+#include "vector_table_naked_ptr_test.h"
+
+#include "test_assert.h"
+#include "vector_table_naked_ptr/vector_table_naked_ptr_generated.h"
+
+namespace flatbuffers {
+namespace tests {
+
+void VectorTableNakedPtrTest() {}
+
+}  // namespace tests
+}  // namespace flatbuffers
