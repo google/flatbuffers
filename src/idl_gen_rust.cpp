419fab82446386614d3d94e318d7242ca6baefcd
Cameron Mulhern <csmulhern@gmail.com>
2025-12-21 00:37:18 -0500

    Declare alloc crate in every Rust namespace
    When performing code generation within a single file, extern crate alloc needs to be delcared to bring alloc into scope within every inner namespace.

--- src/idl_gen_rust.cpp
+++ src/idl_gen_rust.cpp
@@ -423,7 +423,6 @@ class RustGenerator : public BaseGenerator {
     code_.Clear();
     code_ += "// " + std::string(FlatBuffersGeneratedWarning());
     code_ += "// @generated";
-    code_ += "extern crate alloc;";
 
     assert(!cur_name_space_);
 
@@ -3005,6 +3004,7 @@ class RustGenerator : public BaseGenerator {
           "use self::serde::ser::{Serialize, Serializer, SerializeStruct};";
       code_ += "";
     }
+    code_ += "extern crate alloc;";
   }
 
   // Set up the correct namespace. This opens a namespace if the current
